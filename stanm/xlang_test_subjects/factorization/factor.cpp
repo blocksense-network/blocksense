59bb2f6b467335280c765c5d6d7c6c2e9cf0d9d4
Stan Manilov <stan.manilov@metacraft-labs.com>
2024-05-09 14:59:23 +0300

    feat: Add prime factorization in C++
    From the README.md:
    
    "
    Subject program for comparison of the debugging experience from different
    toolchains. Related to task
    https://coda.io/d/_d6vM0kjfQP6#Task-DB_tuk5j/r698&view=modal .
    "

---
+++ stanm/xlang_test_subjects/factorization/factor.cpp
@@ -0,0 +1,103 @@
+#include <cstdlib>
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+const char USAGE[] = "\
+Usage: [program_name] <number>\n\
+    Performs integer factorization on the provided <number> and prints the\n\
+    result as space-separated values. Each prime factor is listed once, and\n\
+    if a factor divides the number more than once, its occurrences are enclosed\n\
+    in parentheses. For example, 2(3) denotes the number being divisible by 2\n\
+    three times.\n\
+";
+
+const char INVALID_NUMBER[] =
+    "The given number could not be parsed or is zero.";
+
+// Prints a single factor of the input number, given whether the factor is the
+// first factor to be printed or not and the number of times it evenly divides
+// the input number (zero or more). If `count` > 0 and `factor_is_first` is set
+// to true, then `factor_is_first` is set to false which has effect outside of
+// this function.
+//
+// Helper function for `print_factors`.
+void print_factor(long factor, bool &factor_is_first, int count) {
+    if (count > 0) {
+        if (!factor_is_first) {
+            cout << ' ';
+        }
+
+        cout << factor;
+
+        if (count > 1) {
+            cout << "(" << count << ")";
+        }
+
+        if (factor_is_first) {
+            factor_is_first = false;
+        }
+    }
+}
+
+// Prints all the positive prime factors of the given number. If the number is
+// negative, prints -1 first.
+void print_factors(long number) {
+    bool factor_is_first = true;
+    if (number < 0) {
+        number *= -1;
+        cout << -1;
+        factor_is_first = false;
+    }
+
+    vector<long> primes;
+
+    for (long factor = 2; factor * factor <= number; ++factor) {
+        bool factor_is_prime = true;
+
+        for (long p : primes) {
+            if (factor % p == 0) {
+                factor_is_prime = false;
+                break;
+            }
+
+            if (p * p > factor) {
+                break;
+            }
+        }
+
+        if (factor_is_prime) {
+            primes.push_back(factor);
+
+            int count = 0;
+            for (; number % factor == 0; number /= factor)
+                ++count;
+
+            print_factor(factor, factor_is_first, count);
+        }
+    }
+
+    if (number > 1) {
+        print_factor(number, factor_is_first, 1);
+    }
+
+    cout << endl;
+}
+
+int main(int argc, char *argv[]) {
+    if (argc != 2) {
+        cout << USAGE << endl;
+        return 1;
+    }
+
+    long number = strtol(argv[1], nullptr, 0);
+    if (number == 0L) {
+        cout << INVALID_NUMBER << endl;
+        return 2;
+    }
+
+    print_factors(number);
+
+    return 0;
+}
