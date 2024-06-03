c246d0e8c2ca64e4914fd7b4b17aec4c086f68f1
Dilyan Dokov <dilyan.dokov@gmail.com>
2024-06-03 15:22:48 +0300

    Support project build on macos

--- nix/shells/pkg-sets/rust.nix
+++ nix/shells/pkg-sets/rust.nix
@@ -1,14 +1,28 @@
 {
   pkgs,
-  self',
+  inputs',
   ...
 }: {
-  packages =
+  packages = with pkgs;
     [
-      self'.legacyPackages.rustToolchain
-    ]
-    ++ (with pkgs; [
+      (with inputs'.fenix.packages;
+        with latest;
+          combine [
+            cargo
+            clippy
+            rust-analyzer
+            rust-src
+            rustc
+            rustfmt
+            targets.wasm32-wasi.latest.rust-std
+          ])
+      clang
+      fermyon-spin
       openssl
       pkg-config
-    ]);
+    ]
+    ++ pkgs.lib.optionals pkgs.stdenv.isDarwin [
+      iconv
+      darwin.apple_sdk.frameworks.Accelerate
+    ];
 }
