7eb8a90b7641f6faaa536816a3edfa654b876330
Hristo Staykov <izo8611@gmail.com>
2024-06-13 20:06:34 +0300

    Refactor to collect futures from all async components

--- apps/sequencer/src/metrics_collector/metrics_collector.rs
+++ apps/sequencer/src/metrics_collector/metrics_collector.rs
@@ -2,32 +2,28 @@ use actix_web::rt::spawn;
 use actix_web::rt::time;
 use prometheus::Encoder;
 use prometheus::TextEncoder;
+use std::io::Error;
 use tokio::time::Duration;
 
 use tracing::info;
 
-pub struct MetricsCollector {}
+pub async fn metrics_collector_loop() -> tokio::task::JoinHandle<Result<(), Error>> {
+    spawn(async move {
+        let mut interval = time::interval(Duration::from_millis(60000));
+        interval.tick().await;
+        loop {
+            let mut buffer = Vec::new();
+            let encoder = TextEncoder::new();
 
-impl MetricsCollector {
-    pub fn new() -> MetricsCollector {
-        spawn(async move {
-            let mut interval = time::interval(Duration::from_millis(60000));
+            // Gather the metrics.
+            let metric_families = prometheus::gather();
+            // Encode them to send.
+            encoder.encode(&metric_families, &mut buffer).unwrap();
+
+            let output = String::from_utf8(buffer.clone()).unwrap();
+            info!("Prometheus metrics:\n{}", output);
+
             interval.tick().await;
-            loop {
-                let mut buffer = Vec::new();
-                let encoder = TextEncoder::new();
-
-                // Gather the metrics.
-                let metric_families = prometheus::gather();
-                // Encode them to send.
-                encoder.encode(&metric_families, &mut buffer).unwrap();
-
-                let output = String::from_utf8(buffer.clone()).unwrap();
-                info!("Prometheus metrics:\n{}", output);
-
-                interval.tick().await;
-            }
-        });
-        MetricsCollector {}
-    }
+        }
+    })
 }
