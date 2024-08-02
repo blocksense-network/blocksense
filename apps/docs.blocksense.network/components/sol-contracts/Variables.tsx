003466dbaa2410f0e942300b2b28555e35c5837c
Kalin Hristov <kalinsky.hristov@gmail.com>
2024-08-02 16:24:05 +0300

    feat(docs.ws): `Variables` Component use `Table` instead of `Accordion`

--- apps/docs.blocksense.network/components/sol-contracts/Variables.tsx
+++ apps/docs.blocksense.network/components/sol-contracts/Variables.tsx
@@ -1,104 +1,100 @@
-import React from 'react';
-import { Badge } from '@/components/ui/badge';
-import {
-  Accordion,
-  AccordionContent,
-  AccordionItem,
-  AccordionTrigger,
-} from '@/components/ui/accordion';
-import { Switch } from '@/components/ui/switch';
-import { Label } from '@/components/ui/label';
+import * as React from 'react';
 
 import { VariableDocItem } from '@blocksense/sol-reflector';
+
+import {
+  Table,
+  TableBody,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from '@/components/ui/table';
 import { ContractItemWrapper } from '@/sol-contracts-components/ContractItemWrapper';
-import { Signature } from '@/sol-contracts-components/Signature';
-import { NatSpec } from '@/sol-contracts-components/NatSpec';
-import { AnchorLinkTitle } from '@/sol-contracts-components/AnchorLinkTitle';
-import { useExpandCollapse } from '@/hooks/useExpandCollapse';
+
+type Column =
+  | 'type'
+  | 'name'
+  | 'description'
+  | 'indexed'
+  | 'mutability'
+  | 'dataLocation';
 
 type VariablesProps = {
   variables?: VariableDocItem[];
   title?: string;
   titleLevel?: 1 | 2 | 3 | 4 | 5 | 6;
+  columns?: Column[];
 };
 
-const getVariableNames = (variables: VariableDocItem[] = []) => {
-  return (
-    variables.map((variable, index) => variable.name || `variable-${index}`) ||
-    []
-  );
+const columnNames = {
+  type: 'Type',
+  name: 'Name',
+  indexed: 'Indexed',
+  description: 'Description',
+  mutability: 'Mutability',
+  dataLocation: 'Data Location',
 };
 
-function getVariableSignature(variable: VariableDocItem): string {
-  const signature: string = variable.signature || '';
-
-  if (variable.constant) {
-    const signatureParts: string[] = signature.split(' ');
-
-    signatureParts.splice(signatureParts.length - 1, 0, 'constant');
-    return signatureParts.join(' ');
+const getParameterValueByColumn = (
+  parameter: VariableDocItem,
+  column: string,
+) => {
+  switch (column) {
+    case 'type':
+      return parameter.typeDescriptions.typeString;
+    case 'name':
+      return parameter.name || parameter._natspecName || 'unnamed';
+    case 'indexed':
+      return parameter.indexed ? 'Yes' : 'No';
+    case 'description':
+      return parameter.natspec.dev || parameter.natspec.notice || '-';
+    case 'mutability':
+      return parameter.mutability || '-';
+    // case 'dataLocation':
+    //   return parameter.dataLocation || '-';
+    default:
+      return '-';
   }
-
-  return signature;
-}
+};
 
 export const Variables = ({
   variables = [],
   title,
   titleLevel,
+  columns = ['type', 'name', 'description'],
 }: VariablesProps) => {
-  const { accordionStates, expandAll, collapseAll, toggleAccordion } =
-    useExpandCollapse(getVariableNames(variables));
-
-  const allExpanded = Object.values(accordionStates).every(
-    accordion => accordion === true,
-  );
-
   return (
     <ContractItemWrapper
       itemsLength={variables?.length}
       title={title}
       titleLevel={titleLevel}
     >
-      <aside className="flex items-center justify-end mt-4 mb-4">
-        <Label htmlFor="expand-collapse-toggle" className="mr-2 ml-2 font-bold">
-          {allExpanded ? 'Collapse All' : 'Expand All'}
-        </Label>
-        <Switch
-          id="expand-collapse-toggle"
-          checked={allExpanded}
-          onCheckedChange={checked => (checked ? expandAll() : collapseAll())}
-        />
-      </aside>
-      <Accordion
-        type="multiple"
-        value={Object.keys(accordionStates).filter(k => accordionStates[k])}
-        className="contract-item-wrapper__variable w-full space-y-4"
-      >
-        {variables?.map((variable, index) => {
-          const id = variable.name || `variable-${index}`;
-          return (
-            <AccordionItem key={id} value={id}>
-              <AccordionTrigger onClick={() => toggleAccordion(id)}>
-                <AnchorLinkTitle
-                  accordion
-                  title={variable.name ? variable.name : 'unnamed'}
-                  titleLevel={6}
-                />
-              </AccordionTrigger>
-              <AccordionContent
-                className={`accordion-content ${accordionStates[id] ? 'expanded' : ''}`}
-              >
-                <Badge className="contract-item-wrapper__variable-mutability">
-                  <span>Mutability: {variable.mutability}</span>
-                </Badge>
-                <Signature signature={getVariableSignature(variable)} />
-                <NatSpec natspec={variable.natspec} />
-              </AccordionContent>
-            </AccordionItem>
-          );
-        })}
-      </Accordion>
+      <Table className="variables__table mt-6 mb-4">
+        <TableHeader className="variables__table-header">
+          <TableRow className="variables__table-header-row">
+            {columns.map(column => (
+              <TableHead className="variables__table-head" key={column}>
+                {columnNames[column]}
+              </TableHead>
+            ))}
+          </TableRow>
+        </TableHeader>
+        <TableBody className="variables__table-body">
+          {variables?.map((variable, index) => (
+            <TableRow className="variables__table-row" key={index}>
+              {columns.map(column => (
+                <TableCell
+                  className={`variables__table-cell variables__table-cell--${column}`}
+                  key={column}
+                >
+                  {getParameterValueByColumn(variable, column)}
+                </TableCell>
+              ))}
+            </TableRow>
+          ))}
+        </TableBody>
+      </Table>
     </ContractItemWrapper>
   );
 };
