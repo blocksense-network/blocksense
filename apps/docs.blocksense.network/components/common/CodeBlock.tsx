20361ce7125b7ed0dc4d87b83c57db454c34fe68
Emil Ivanichkov <emil.ivanichkov@gmail.com>
2024-08-08 23:48:03 +0300

    feat(docs.ws): Enhance CodeBlock with CopyButton and custom styling options
    - Added `CopyButton` to the CodeBlock component for easy copying of code snippets.
    - Introduced `precompiledHtml` prop in CodeBlock for rendering precompiled HTML, improving performance.
    - Added `copyButtonClasses` prop in CopyButton for custom styling flexibility.
    - Refactored corresponding components to support new features.

--- apps/docs.blocksense.network/components/common/CodeBlock.tsx
+++ apps/docs.blocksense.network/components/common/CodeBlock.tsx
@@ -1,30 +1,46 @@
 import React from 'react';
 import { codeToHtml } from 'shiki';
+import { CopyButton } from './CopyButton';
 
 type CodeBlockProps = {
   code: string;
+  precompiledHtml?: string;
   lang?: string;
   theme?: string;
+  copy?: boolean;
 };
 
 export const CodeBlock = ({
-  code = '',
+  code,
+  precompiledHtml = '',
   lang = 'text',
   theme = 'material-theme-lighter',
+  copy = true,
 }: CodeBlockProps) => {
   const [html, setHtml] = React.useState('');
 
   React.useEffect(() => {
-    codeToHtml(code, {
-      lang,
-      theme,
-    }).then((htmlString: any) => setHtml(htmlString));
+    precompiledHtml
+      ? setHtml(precompiledHtml)
+      : codeToHtml(code, {
+          lang,
+          theme,
+        }).then((htmlString: any) => setHtml(htmlString));
   }, [code]);
 
   return (
-    <div
-      className="signature__code flex-grow mr-2"
-      dangerouslySetInnerHTML={{ __html: html }}
-    />
+    <div style={{ position: 'relative' }}>
+      {copy && (
+        <CopyButton
+          textToCopy={code}
+          tooltipPosition="left"
+          copyButtonClasses="absolute top-0 right-0 m-2 nx-z-10"
+        />
+      )}
+      <div
+        className="signature__code flex-grow"
+        dangerouslySetInnerHTML={{ __html: html }}
+      />
+    </div>
   );
 };
