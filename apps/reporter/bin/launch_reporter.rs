985c3ad3f877ab8a2685dc900bde8edb0a14190d
Antoni Dikov <antoni.dikov@gmail.com>
2024-08-21 15:07:53 +0300

    feat(node): Parallelize data feed reporter

--- apps/reporter/bin/launch_reporter.rs
+++ apps/reporter/bin/launch_reporter.rs
@@ -2,5 +2,13 @@ use data_feeds::orchestrator::orchestrator;
 
 #[tokio::main]
 async fn main() {
+    // This trigger spawns threads, which Ctrl+C does not kill.  So
+    // for this case we need to detect Ctrl+C and shut those threads
+    // down.  For simplicity, we do this by terminating the process.
+    tokio::spawn(async move {
+        tokio::signal::ctrl_c().await.unwrap();
+        std::process::exit(0);
+    });
+
     orchestrator().await;
 }
