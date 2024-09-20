680fd19ec2d26c96abfc39dd8ee87bc225a726ca
Petar Kirov <petar.p.kirov@gmail.com>
2024-09-20 15:53:09 +0300

    wip3

--- apps/data-feeds-config-generator/src/data-services/chainlink_feeds.ts
+++ apps/data-feeds-config-generator/src/data-services/chainlink_feeds.ts
@@ -1,7 +1,19 @@
+import { Web3, FMT_NUMBER, FMT_BYTES } from 'web3';
+
 import { selectDirectory } from '@blocksense/base-utils/fs';
 
+import {
+  NetworkName,
+  EthereumAddress,
+  parseEthereumAddress,
+  isZeroAddress,
+} from '@blocksense/base-utils/evm-utils';
+
+import {
+  ConfirmedFeedEvent,
+  decodeConfirmedFeedEvent,
+} from '../chainlink-compatibility/types';
 import { RawDataFeeds, decodeChainLinkFeedsInfo } from './types';
-import { artifactsDir } from '../paths';
 
 export async function collectRawDataFeeds(directoryPath: string) {
   const { readAllJSONFiles } = selectDirectory(directoryPath);
@@ -22,4 +34,136 @@ export async function collectRawDataFeeds(directoryPath: string) {
   }
 
   return rawDataFeeds;
+}
+
+export const feedRegistries: {
+  [key in NetworkName]?: EthereumAddress;
+} = {
+  'ethereum-mainnet': parseEthereumAddress(
+    '0x47Fb2585D2C56Fe188D0E6ec628a38b74fCeeeDf',
+  ),
+};
+
+export type FeedRegistryEventsPerAggregator = {
+  [aggregator: EthereumAddress]: ConfirmedFeedEvent['returnValues'];
+};
+
+export async function getAllProposedFeedsInRegistry(
+  web3: Web3,
+  network: NetworkName,
+): Promise<FeedRegistryEventsPerAggregator> {
+  const registryAddress = feedRegistries[network];
+  if (!registryAddress) {
+    throw new Error(`No feed registry found for network ${network}`);
+  }
+  return await getAllProposedFeedsRegistryEvents(web3, registryAddress);
+}
+
+export async function getAllProposedFeedsRegistryEvents(
+  web3: Web3,
+  feedRegistryAddress: EthereumAddress,
+): Promise<FeedRegistryEventsPerAggregator> {
+  const abi = [
+    {
+      anonymous: false,
+      inputs: [
+        {
+          indexed: true,
+          internalType: 'address',
+          name: 'asset',
+          type: 'address',
+        },
+        {
+          indexed: true,
+          internalType: 'address',
+          name: 'denomination',
+          type: 'address',
+        },
+        {
+          indexed: true,
+          internalType: 'address',
+          name: 'latestAggregator',
+          type: 'address',
+        },
+        {
+          indexed: false,
+          internalType: 'address',
+          name: 'previousAggregator',
+          type: 'address',
+        },
+        {
+          indexed: false,
+          internalType: 'uint16',
+          name: 'nextPhaseId',
+          type: 'uint16',
+        },
+        {
+          indexed: false,
+          internalType: 'address',
+          name: 'sender',
+          type: 'address',
+        },
+      ],
+      name: 'FeedConfirmed',
+      type: 'event',
+    },
+    {
+      anonymous: false,
+      inputs: [
+        {
+          indexed: true,
+          internalType: 'address',
+          name: 'asset',
+          type: 'address',
+        },
+        {
+          indexed: true,
+          internalType: 'address',
+          name: 'denomination',
+          type: 'address',
+        },
+        {
+          indexed: true,
+          internalType: 'address',
+          name: 'proposedAggregator',
+          type: 'address',
+        },
+        {
+          indexed: false,
+          internalType: 'address',
+          name: 'currentAggregator',
+          type: 'address',
+        },
+        {
+          indexed: false,
+          internalType: 'address',
+          name: 'sender',
+          type: 'address',
+        },
+      ],
+      name: 'FeedProposed',
+      type: 'event',
+    },
+  ] as const;
+
+  const registryContract = new web3.eth.Contract(abi, feedRegistryAddress);
+
+  const proposedFeeds = (
+    await registryContract.getPastEvents(
+      'FeedConfirmed',
+      {
+        fromBlock: 0,
+        toBlock: 'latest',
+      },
+      {
+        number: FMT_NUMBER.BIGINT,
+        bytes: FMT_BYTES.HEX,
+      },
+    )
+  )
+    .map(f => decodeConfirmedFeedEvent(f).returnValues)
+    .filter(ev => !isZeroAddress(ev.latestAggregator))
+    .map(ev => [ev.latestAggregator, ev]);
+
+  return Object.fromEntries(proposedFeeds);
 }
