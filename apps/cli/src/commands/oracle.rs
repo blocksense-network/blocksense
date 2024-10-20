15b24fb52b23f1573775ef1bf2f6b9cc3b190448
Stefan Nikolov <consultsen.stefan@gmail.com>
2024-10-20 19:05:25 +0300

    feat(cli): Write dummy BlocksenseConfig to new spin component

--- apps/cli/src/commands/oracle.rs
+++ apps/cli/src/commands/oracle.rs
@@ -1,10 +1,14 @@
 use std::path::PathBuf;
 use std::process::Stdio;
+use std::{env, fs};
 
 use anyhow::Result;
 
+use blocksense_registry::config::get_blocksense_config_dummy;
 use clap::{Parser, Subcommand};
-use tokio::process::Command;
+use tokio::io::{AsyncBufReadExt, BufReader, BufWriter};
+use tokio::{fs::File, process::Command};
+use tracing::error;
 
 /// Commands for working with the capabilities.
 #[derive(Debug, Subcommand)]
@@ -96,15 +100,44 @@ pub struct Init {
 
 impl Init {
     pub async fn run(self) -> Result<()> {
-        Command::new("spin")
+        let current_dir = env::current_dir()?;
+
+        let mut child = Command::new("spin")
             .arg("new")
             .arg("-t")
-            .arg("oracle-rust")
-            .stdout(Stdio::inherit())
+            // .arg("oracle-rust") //TODO(snikolov): There is no template oracle-rust
+            .arg("http-rust")
+            .stdout(Stdio::piped())
             .stderr(Stdio::inherit())
-            .spawn()?
-            .wait()
-            .await?;
+            .spawn()?;
+
+        if let Some(stdout) = child.stdout.take() {
+            let reader = BufReader::new(stdout);
+            let mut lines = reader.lines();
+
+            // First line of the `spin new` command is the name of the spin component and its' folder
+            if let Ok(Some(relative_directory)) = lines.next_line().await {
+                let dir_path = current_dir.join(&relative_directory);
+                if dir_path.exists() {
+                    let blocksense_config_path = dir_path.join("blocksense-config.json");
+
+                    // Create and write to the temp file
+                    let mut config_file = File::create(&blocksense_config_path).await?;
+                    let config_json = serde_json::to_string_pretty(&get_blocksense_config_dummy())?;
+
+                    fs::write(&blocksense_config_path, config_json)?;
+
+                    println!(
+                        "Dummy BlocksenseConfig generated at: {:?}",
+                        blocksense_config_path
+                    );
+                } else {
+                    error!("Directory does not exist: {}", relative_directory);
+                }
+            }
+        }
+
+        let status = child.wait().await?;
 
         Ok(())
     }
