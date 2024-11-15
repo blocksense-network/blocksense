80585ea3b88c10a57212cf11321fe05aa3d97b19
Kalin Hristov <kalinsky.hristov@gmail.com>
2024-11-15 15:35:29 +0200

    feat(docs.ws): `Next.js` successfully prerenders `Reference Documentation > Overview` Page

---
+++ apps/docs.blocksense.network/components/ReferenceDocumentation/Overview.tsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import { ContractAnchorLink } from '@/components/sol-contracts/ContractAnchorLink';
+
+export const Overview = () => {
+  return (
+    <ul className="overview__list nx-mt-6 nx-list-none first:nx-mt-0 ltr:nx-ml-0 rtl:nx-mr-6">
+      <ContractAnchorLink label="ProxyCall" />
+      <ContractAnchorLink label="IChainlinkFeedRegistry" />
+      <ContractAnchorLink label="IChainlinkAggregator" />
+      <ContractAnchorLink label="IFeedRegistry" />
+      <ContractAnchorLink label="IAggregator" />
+      <ContractAnchorLink label="FeedRegistry" />
+      <ContractAnchorLink label="ChainlinkProxy" />
+      <ContractAnchorLink label="UpgradeableProxy" />
+      <ContractAnchorLink label="HistoricDataFeedStoreV2" />
+      <ContractAnchorLink label="HistoricDataFeedStoreV1" />
+      <ContractAnchorLink label="DataFeedStoreV3" />
+      <ContractAnchorLink label="DataFeedStoreV2" />
+      <ContractAnchorLink label="DataFeedStoreV1" />
+    </ul>
+  );
+};
