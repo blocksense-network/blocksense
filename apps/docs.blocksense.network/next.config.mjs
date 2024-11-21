817c1645e7d539a925c91688480cc11877dd4f44
Kalin Hristov <kalinsky.hristov@gmail.com>
2024-11-21 11:50:02 +0200

    feat(docs.ws): Update `next.config.mjs`
    Update the content of the `next.config.mjs` file to address `log errors.`

--- apps/docs.blocksense.network/next.config.mjs
+++ apps/docs.blocksense.network/next.config.mjs
@@ -9,11 +9,5 @@ import nextra from 'nextra';
 const withNextra = nextra({
   theme: 'nextra-theme-docs',
   themeConfig: './theme.config.tsx',
-  latex: true,
-  search: {
-    codeblocks: false,
-  },
 });
-export default withNextra({
-  reactStrictMode: true,
-});
+export default withNextra({});
