b574f7bceef18fae4286eb971552a6a5f1139672
Kalin Hristov <kalinsky.hristov@gmail.com>
2024-12-10 17:30:15 +0200

    feat(docs.ws): `Data Feeds > Overview` page is active
    Implement `Data Feeds > Overview` page as a `statically prerendered RSC`

--- apps/docs.blocksense.network/components/sol-contracts/ContractAddress.tsx
+++ apps/docs.blocksense.network/components/sol-contracts/ContractAddress.tsx
@@ -1,6 +1,6 @@
 import * as React from 'react';
 import Link from 'next/link';
-import { useRouter } from 'next/router';
+import { useRouter } from 'next/navigation';
 
 import { Tooltip } from '@/components/common/Tooltip';
 import { CopyButton } from '@/components/common/CopyButton';
