6f7fa32e95db7c3560a5de6defde9b1be3ff4347
Kalin Hristov <kalinsky.hristov@gmail.com>
2025-01-08 17:43:07 +0200

    rebase(docs.ws): Resolve issues with the `Reference Documentation`

--- apps/docs.blocksense.network/components/ReferenceDocumentation/Overview.tsx
+++ apps/docs.blocksense.network/components/ReferenceDocumentation/Overview.tsx
@@ -4,19 +4,15 @@ import { ContractAnchorLink } from '@/sol-contracts-components/ContractAnchorLin
 export const Overview = () => {
   return (
     <ul className="overview__list nx-mt-6 nx-list-none first:nx-mt-0 ltr:nx-ml-0 rtl:nx-mr-6">
-      <ContractAnchorLink label="ProxyCall" />
+      <ContractAnchorLink label="Blocksense" />
       <ContractAnchorLink label="IChainlinkFeedRegistry" />
       <ContractAnchorLink label="IChainlinkAggregator" />
-      <ContractAnchorLink label="IFeedRegistry" />
-      <ContractAnchorLink label="IAggregator" />
-      <ContractAnchorLink label="FeedRegistry" />
-      <ContractAnchorLink label="ChainlinkProxy" />
-      <ContractAnchorLink label="UpgradeableProxy" />
-      <ContractAnchorLink label="HistoricDataFeedStoreV2" />
-      <ContractAnchorLink label="HistoricDataFeedStoreV1" />
-      <ContractAnchorLink label="DataFeedStoreV3" />
-      <ContractAnchorLink label="DataFeedStoreV2" />
-      <ContractAnchorLink label="DataFeedStoreV1" />
+      <ContractAnchorLink label="ICLFeedRegistryAdapter" />
+      <ContractAnchorLink label="ICLAggregatorAdapter" />
+      <ContractAnchorLink label="CLFeedRegistryAdapter" />
+      <ContractAnchorLink label="CLAggregatorAdapter" />
+      <ContractAnchorLink label="AggregatedDataFeedStore" />
+      <ContractAnchorLink label="AccessControl" />
     </ul>
   );
 };
