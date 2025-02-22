be4b72e4f87a2470e42662b9ed6ebe829887f688
Martin Ivanov <xeartybg@gmail.com>
2025-02-22 02:33:50 +0200

    Implement price fetching from Bitget

---
+++ apps/oracles/crypto-price-feeds/src/exchanges/bitget.rs
@@ -0,0 +1,41 @@
+use anyhow::Result;
+
+use futures::FutureExt;
+use serde::Deserialize;
+
+use crate::{
+    common::{http_get_json, PairPriceData},
+    traits::prices_fetcher::PricesFetcher,
+};
+
+#[derive(Default, Debug, Clone, PartialEq, Deserialize)]
+pub struct BitgetPriceData {
+    pub symbol: String,
+    pub close: String,
+}
+
+#[derive(Default, Debug, Clone, PartialEq, Deserialize)]
+pub struct BitgetPriceResponse {
+    pub code: String,
+    pub data: Vec<BitgetPriceData>,
+}
+pub struct BitgetFetcher;
+
+impl PricesFetcher for BitgetFetcher {
+    fn fetch(&self) -> futures::future::LocalBoxFuture<Result<PairPriceData>> {
+        async {
+            let response = http_get_json::<BitgetPriceResponse>(
+                "https://api.bitget.com/api/spot/v1/market/tickers",
+                None,
+            )
+            .await?;
+
+            Ok(response
+                .data
+                .into_iter()
+                .map(|value| (value.symbol, value.close))
+                .collect())
+        }
+        .boxed_local()
+    }
+}
