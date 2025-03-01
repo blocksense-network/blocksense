b82e8c780cc0060358b58fb136e8a7849b5af574
Petar Kirov <petar.p.kirov@gmail.com>
2025-03-01 10:55:01 +0200

    refactor(base-utils): Return `null` instead of `''` from `getEnvStringNotAssert`

--- libs/ts/base-utils/src/env/functions.ts
+++ libs/ts/base-utils/src/env/functions.ts
@@ -21,11 +21,11 @@ export function getOptionalEnvString(
   return process.env[varName] ?? defaultValue;
 }
 
-export function getEnvStringNotAssert(varName: string): string {
+export function getEnvStringNotAssert(varName: string): string | null {
   const value = process.env[varName];
   if (!value || value.trim() === '') {
     console.warn(`Env variable '${varName}' is missing or empty.`);
-    return '';
+    return null;
   }
   return value;
 }
