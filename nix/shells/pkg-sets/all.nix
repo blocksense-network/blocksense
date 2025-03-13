c224d7fdb2ef9327e3a290b77101b62e9787663e
Galin Stefanov <galinstef10@gmail.com>
2025-03-13 15:39:16 +0200

    config(shells): Add anvil only for linux

--- nix/shells/pkg-sets/all.nix
+++ nix/shells/pkg-sets/all.nix
@@ -1,18 +1,22 @@
 {
+  pkgs,
   self,
   config,
+  lib,
   ...
 }:
+let
+  inherit (pkgs.hostPlatform) isLinux;
+in
 {
   imports = [
     ./js.nix
     ./rust.nix
-    ./anvil.nix
     ./kafka.nix
 
     self.nixosModules.blocksense-process-compose
     ../../test-environments/example-setup-01.nix
-  ];
+  ] ++ lib.optional isLinux ./anvil.nix;
 
   enterShell = ''
     ln -fs ${config.process.managers.process-compose.configFile} ${config.devenv.root}/process-compose.yml
