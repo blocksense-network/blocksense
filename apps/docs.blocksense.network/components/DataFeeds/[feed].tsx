810c5fda09062d731c46aba4965659352c67bb4b
Emil Ivanichkov <emil.ivanichkov@gmail.com>
2025-03-26 22:14:08 +0200

    feat(docs.ws): Initial version of feed pages with v2 config

--- apps/docs.blocksense.network/components/DataFeeds/[feed].tsx
+++ apps/docs.blocksense.network/components/DataFeeds/[feed].tsx
@@ -1,20 +1,22 @@
 import DATA_FEEDS from '@blocksense/data-feeds-config-generator/feeds_config';
 import CONTRACTS_DEPLOYMENT_CONFIG from '@blocksense/data-feeds-config-generator/evm_contracts_deployment_v1';
-import { decodeFeedsConfig } from '@blocksense/config-types/data-feeds-config';
+import { decodeNewFeedsConfig } from '@blocksense/config-types/data-feeds-config';
 import {
   CLAggregatorAdapterData,
   decodeDeploymentConfigV1,
 } from '@blocksense/config-types/evm-contracts-deployment';
 
+import { capitalizeWords, showMsInSeconds, showPercentage } from '@/src/utils';
 import { Error404 } from '@/components/common/Error404';
 import { DataFeedCardSection } from '@/components/DataFeeds/DataFeedCardSection';
 import { DataFeedCardContentItem } from '@/components/DataFeeds/DataFeedCardContentItem';
 import { ContractAddress } from '@/components/sol-contracts/ContractAddress';
 import { CopyButton } from '@blocksense/ui/CopyButton';
 import { QuestionsCardContent } from '@/components/DataFeeds/QuestionsCardContent';
+import { prepareDeploymentData } from '@/src/data-feeds/x';
 
 export function generateStaticParams() {
-  const feedsConfig = decodeFeedsConfig(DATA_FEEDS);
+  const feedsConfig = decodeNewFeedsConfig(DATA_FEEDS);
   return feedsConfig.feeds.map(feed => ({
     feed: String(feed.id),
   }));
@@ -33,7 +35,7 @@ export default async function DataFeed({ params }: DataFeedProps) {
     return <Error404 />;
   }
 
-  const feedsConfig = decodeFeedsConfig(DATA_FEEDS);
+  const feedsConfig = decodeNewFeedsConfig(DATA_FEEDS);
   // const feedsDeploymentInfo = decodeDeploymentConfigV1(
   //   CONTRACTS_DEPLOYMENT_CONFIG,
   // )['ethereum-sepolia']?.contracts?.CLAggregatorAdapter;
@@ -54,16 +56,6 @@ export default async function DataFeed({ params }: DataFeedProps) {
   //   return <Error404 />;
   // }
 
-  const {
-    id,
-    description,
-    decimals,
-    pair,
-    report_interval_ms,
-    quorum_percentage,
-    type,
-  } = feed;
-
   // const { base, quote, address } = feedDeploymentInfo;
 
   // const feedRegistry = {
@@ -73,9 +65,9 @@ export default async function DataFeed({ params }: DataFeedProps) {
   //         <div className="flex gap-2 justify-between items-center break-all">
   //           Feed id:
   //           <span className="flex gap-2 items-center">
-  //             <code className="inline">{id}</code>
+  //             <code className="inline">{feed.id}</code>
   //             <CopyButton
-  //               textToCopy={`${id}`}
+  //               textToCopy={`${feed.id}`}
   //               tooltipPosition="top"
   //               copyButtonClasses="translate-x-1"
   //               background={false}
@@ -118,52 +110,78 @@ export default async function DataFeed({ params }: DataFeedProps) {
   //   ),
   // };
 
-  const dataFeedCardArray = [
-    {
-      title: 'Core Properties',
-      description:
-        'Define elements of a single price feed and provide transparency, reliability, and security in data updates',
-      items: [
-        { label: 'Name', value: description },
-        { label: 'Feed ID', value: id },
-        { label: 'Quorum Percentage', value: `${quorum_percentage * 100}%` },
-      ],
-    },
-    {
-      title: 'Price Feed Properties',
-      description:
-        'Outline properties of a price feed, such as key features and guarantee consistent, timely updates',
-      items: [
-        { label: 'Pair', value: `${pair.base} / ${pair.quote}` },
-        { label: 'Decimals', value: decimals },
-        {
-          label: 'Report Interval',
-          value: `${(report_interval_ms / 1000).toFixed(2)} seconds`,
-        },
-        {
-          label: 'Category',
-          value: typeof type === 'string' ? type : String(type),
-        },
-        { label: 'Data Providers', value: 'No information yet' },
-      ],
-    },
-  ];
+  const dataFeedInfo = {
+    title: 'Data Feed Info',
+    description: `Description: ${feed.description}`,
+    items: [
+      { label: 'Base', value: feed.additional_feed_info.pair.base },
+      { label: 'Quote', value: feed.additional_feed_info.pair.quote },
+      { label: 'Feed Type', value: capitalizeWords(feed.type) },
+      { label: 'Categoty', value: feed.additional_feed_info.category },
+    ],
+  };
+
+  const priceFeedInfo =
+    feed.type === 'price-feed'
+      ? {
+          title: 'Price Feed Default Configuration',
+          description: 'Default configuration for the price data feed.',
+          items: [
+            {
+              label: 'Decimals',
+              value: feed.additional_feed_info.decimals,
+            },
+            {
+              label: 'Aggregator method',
+              value: capitalizeWords(feed.quorum.aggregation),
+            },
+            {
+              label: 'Report Interval',
+              value: showMsInSeconds(feed.schedule.interval_ms),
+            },
+            {
+              label: 'Heartbeat',
+              value: showMsInSeconds(feed.schedule.heartbeat_ms),
+            },
+            {
+              label: 'Deviation Percentage',
+              value: showPercentage(feed.schedule.deviation_percentage),
+            },
+          ],
+        }
+      : null;
 
   return (
     <div className="data-feed-details">
       <h1 className="text-2xl font-bold text-gray-900 mt-10 dark:text-white">
-        {description} | ID: {id}
+        {feed.full_name} | ID: {feed.id}
       </h1>
 
       <div className="grid grid-cols-1 lg:grid-cols-2 gap-4 w-full lg:w-[42rem] xl:w-[45rem]">
-        {dataFeedCardArray.map((section, index) => (
+        <DataFeedCardSection
+          key={'dataFeedInfo'}
+          title={dataFeedInfo.title}
+          description={dataFeedInfo.description}
+        >
+          <div className="data-feed-card-content grid grid-cols-2 gap-4">
+            {dataFeedInfo.items.map((item, idx) => (
+              <DataFeedCardContentItem
+                key={idx}
+                label={item.label}
+                value={item.value}
+              />
+            ))}
+          </div>
+        </DataFeedCardSection>
+
+        {priceFeedInfo && (
           <DataFeedCardSection
-            key={index}
-            title={section.title}
-            description={section.description}
+            key={'priceFeedInfo'}
+            title={priceFeedInfo.title}
+            description={priceFeedInfo.description}
           >
             <div className="data-feed-card-content grid grid-cols-2 gap-4">
-              {section.items.map((item, idx) => (
+              {priceFeedInfo.items.map((item, idx) => (
                 <DataFeedCardContentItem
                   key={idx}
                   label={item.label}
@@ -172,7 +190,7 @@ export default async function DataFeed({ params }: DataFeedProps) {
               ))}
             </div>
           </DataFeedCardSection>
-        ))}
+        )}
         {/* <DataFeedCardSection
           key={`evm-access-info`}
           title="EVM Access Info"
@@ -224,8 +242,8 @@ export default async function DataFeed({ params }: DataFeedProps) {
               />
             </div>
           )}
-        </DataFeedCardSection>
-        <DataFeedCardSection
+        </DataFeedCardSection> */}
+        {/* <DataFeedCardSection
           key={`questions`}
           title="Questions?"
           description="If you have any questions speak with our team on one of the following platforms:"
--- apps/docs.blocksense.network/components/DataFeeds/columns.tsx
+++ apps/docs.blocksense.network/components/DataFeeds/columns.tsx
@@ -3,6 +3,7 @@ import { Tooltip } from '@blocksense/ui/Tooltip';
 import { DataTableColumnHeader } from '@/components/common/DataTable/DataTableColumnHeader';
 import { DataTableBadge } from '@/components/common/DataTable/DataTableBadge';
 import { ColumnDef } from '@/components/common/DataTable/dataTableUtils';
+import { capitalizeWords } from '@/src/utils';
 
 export const columns: ColumnDef[] = [
   {
@@ -13,25 +14,54 @@ export const columns: ColumnDef[] = [
     facetedFilter: true,
   },
   {
-    id: 'description',
+    id: 'name',
     title: 'Data Feed Name',
     header: ({ column }) => <DataTableColumnHeader title={column.title} />,
     cell: ({ row }) => (
       <Tooltip position="right">
-        <Tooltip.Content>Data Feed Info</Tooltip.Content>
-        <DataTableBadge>{row.description}</DataTableBadge>
+        <Tooltip.Content>{row.description}</Tooltip.Content>
+        <DataTableBadge>{row.full_name}</DataTableBadge>
       </Tooltip>
+    ),
+  },
+  {
+    id: 'base',
+    title: 'Base',
+    header: ({ column }) => <DataTableColumnHeader title={column.title} />,
+    cell: ({ row }) => (
+      <DataTableBadge>{row.additional_feed_info.pair.base}</DataTableBadge>
+    ),
+  },
+  {
+    id: 'quote',
+    title: 'Quote',
+    header: ({ column }) => <DataTableColumnHeader title={column.title} />,
+    cell: ({ row }) => (
+      <DataTableBadge>{row.additional_feed_info.pair.quote}</DataTableBadge>
     ),
   },
   {
     id: 'decimals',
     title: 'Decimals',
     header: ({ column }) => <DataTableColumnHeader title={column.title} />,
-    cell: ({ row }) => <DataTableBadge>{row.decimals}</DataTableBadge>,
+    cell: ({ row }) => (
+      <DataTableBadge>{row.additional_feed_info.decimals}</DataTableBadge>
+    ),
   },
   {
-    id: 'report_interval_ms',
-    title: 'Refresh Interval (ms)',
+    id: 'type',
+    title: 'Feed Type',
     header: ({ column }) => <DataTableColumnHeader title={column.title} />,
+    cell: ({ row }) => (
+      <DataTableBadge>{capitalizeWords(row.type)}</DataTableBadge>
+    ),
+  },
+  {
+    id: 'category',
+    title: 'Category',
+    header: ({ column }) => <DataTableColumnHeader title={column.title} />,
+    cell: ({ row }) => (
+      <DataTableBadge>{row.additional_feed_info.category}</DataTableBadge>
+    ),
   },
 ];
