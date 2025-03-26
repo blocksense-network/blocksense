a29f5a853030cab44ed65055913cf7b3a136f485
Emil Ivanichkov <emil.ivanichkov@gmail.com>
2025-03-26 20:56:30 +0200

    refactor(docs.ws): Move `capitalizeWords` in `util.ts`
    This way it is reusable

--- apps/docs.blocksense.network/src/utils.ts
+++ apps/docs.blocksense.network/src/utils.ts
@@ -52,3 +52,10 @@ export function previewHexStringOrDefault(
 ): string {
   return value ? previewHexString(value, bytesToShow) : defaultValue;
 }
+
+export const capitalizeWords = (input: string): string => {
+  return input
+    .split('-')
+    .map(word => word.charAt(0).toUpperCase() + word.slice(1))
+    .join(' ');
+};
