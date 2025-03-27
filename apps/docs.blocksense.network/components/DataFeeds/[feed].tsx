40ca8aabe94cc2a0210f11bed60937af1bbccc29
Emil Ivanichkov <emil.ivanichkov@gmail.com>
2025-03-27 21:21:04 +0200

    feat(docs.ws): Integrate feed config v2 to Feed pages

--- apps/docs.blocksense.network/components/DataFeeds/[feed].tsx
+++ apps/docs.blocksense.network/components/DataFeeds/[feed].tsx
@@ -1,19 +1,14 @@
 import DATA_FEEDS from '@blocksense/data-feeds-config-generator/feeds_config';
-import CONTRACTS_DEPLOYMENT_CONFIG from '@blocksense/data-feeds-config-generator/evm_contracts_deployment_v1';
 import { decodeNewFeedsConfig } from '@blocksense/config-types/data-feeds-config';
-import {
-  CLAggregatorAdapterData,
-  decodeDeploymentConfigV1,
-} from '@blocksense/config-types/evm-contracts-deployment';
 
-import { capitalizeWords, showMsInSeconds, showPercentage } from '@/src/utils';
 import { Error404 } from '@/components/common/Error404';
-import { DataFeedCardSection } from '@/components/DataFeeds/DataFeedCardSection';
-import { DataFeedCardContentItem } from '@/components/DataFeeds/DataFeedCardContentItem';
-import { ContractAddress } from '@/components/sol-contracts/ContractAddress';
-import { CopyButton } from '@blocksense/ui/CopyButton';
-import { QuestionsCardContent } from '@/components/DataFeeds/QuestionsCardContent';
-import { prepareDeploymentData } from '@/src/data-feeds/x';
+import { CoreConfigCard } from '@/components/DataFeeds/Cards/CoreConfigCard';
+import { PriceFeedConfigCard } from '@/components/DataFeeds/Cards/PriceFeedConfigCard';
+import { ConsensusConfigCard } from '@/components/DataFeeds/Cards/ConsensusConfigCard';
+import { NetworkAccessCard } from '@/components/DataFeeds/Cards/NetworkAccessCard';
+import { OracleConfigCard } from '@/components/DataFeeds/Cards/OracleConfigCard';
+
+import { prepareDeploymentData } from '@/src/deployed-contracts/utils';
 
 export function generateStaticParams() {
   const feedsConfig = decodeNewFeedsConfig(DATA_FEEDS);
@@ -36,220 +31,34 @@ export default async function DataFeed({ params }: DataFeedProps) {
   }
 
   const feedsConfig = decodeNewFeedsConfig(DATA_FEEDS);
-  // const feedsDeploymentInfo = decodeDeploymentConfigV1(
-  //   CONTRACTS_DEPLOYMENT_CONFIG,
-  // )['ethereum-sepolia']?.contracts?.CLAggregatorAdapter;
   const feed = feedsConfig.feeds.find(feed => feed.id === Number(feedId));
 
   if (!feed) {
     return <Error404 />;
   }
 
-  // const feedDeploymentInfo = feedsDeploymentInfo?.find(
-  //   (info: CLAggregatorAdapterData) => info.description === feed.description,
-  // );
+  const feedsDeploymentInfo = await prepareDeploymentData(feed.full_name);
 
-  // if (!feedDeploymentInfo) {
-  //   console.error(
-  //     `No deployment info found for feed: ${feed.description} (${feed.id})`,
-  //   );
-  //   return <Error404 />;
-  // }
-
-  // const { base, quote, address } = feedDeploymentInfo;
-
-  // const feedRegistry = {
-  //   directAccess: (
-  //     <div className="text-sm text-gray-500 ml-2">
-  //       {
-  //         <div className="flex gap-2 justify-between items-center break-all">
-  //           Feed id:
-  //           <span className="flex gap-2 items-center">
-  //             <code className="inline">{feed.id}</code>
-  //             <CopyButton
-  //               textToCopy={`${feed.id}`}
-  //               tooltipPosition="top"
-  //               copyButtonClasses="translate-x-1"
-  //               background={false}
-  //             />
-  //           </span>
-  //         </div>
-  //       }
-  //     </div>
-  //   ),
-  //   chainlinkStyleRegistry:
-  //     base && quote ? (
-  //       <div className="text-sm text-gray-500 ml-2">
-  //         <div className="flex flex-col gap-2 justify-between break-all">
-  //           base:{' '}
-  //           <ContractAddress
-  //             address={base}
-  //             abbreviation={{ hasAbbreviation: true, bytesToShow: 4 }}
-  //             copyButton={{ enableCopy: true, background: false }}
-  //           />
-  //         </div>
-  //         <div className="flex flex-col gap-2 justify-between break-all">
-  //           quote:{''}
-  //           <ContractAddress
-  //             address={quote}
-  //             abbreviation={{ hasAbbreviation: true, bytesToShow: 4 }}
-  //             copyButton={{ enableCopy: true, background: false }}
-  //           />
-  //         </div>
-  //       </div>
-  //     ) : undefined,
-  //   aggregatorProxyAddress: (
-  //     <div className="text-sm text-gray-500 ml-2 flex flex-col gap-2 justify-between break-all">
-  //       address:{' '}
-  //       <ContractAddress
-  //         address={address}
-  //         abbreviation={{ hasAbbreviation: true, bytesToShow: 4 }}
-  //         copyButton={{ enableCopy: true, background: false }}
-  //       />
-  //     </div>
-  //   ),
-  // };
-
-  const dataFeedInfo = {
-    title: 'Data Feed Info',
-    description: `Description: ${feed.description}`,
-    items: [
-      { label: 'Base', value: feed.additional_feed_info.pair.base },
-      { label: 'Quote', value: feed.additional_feed_info.pair.quote },
-      { label: 'Feed Type', value: capitalizeWords(feed.type) },
-      { label: 'Categoty', value: feed.additional_feed_info.category },
-    ],
-  };
-
-  const priceFeedInfo =
-    feed.type === 'price-feed'
-      ? {
-          title: 'Price Feed Default Configuration',
-          description: 'Default configuration for the price data feed.',
-          items: [
-            {
-              label: 'Decimals',
-              value: feed.additional_feed_info.decimals,
-            },
-            {
-              label: 'Aggregator method',
-              value: capitalizeWords(feed.quorum.aggregation),
-            },
-            {
-              label: 'Report Interval',
-              value: showMsInSeconds(feed.schedule.interval_ms),
-            },
-            {
-              label: 'Heartbeat',
-              value: showMsInSeconds(feed.schedule.heartbeat_ms),
-            },
-            {
-              label: 'Deviation Percentage',
-              value: showPercentage(feed.schedule.deviation_percentage),
-            },
-          ],
-        }
-      : null;
+  if (!feedsDeploymentInfo) {
+    return <Error404 />;
+  }
 
   return (
-    <div className="data-feed-details">
-      <h1 className="text-2xl font-bold text-gray-900 mt-10 dark:text-white">
+    <div className="data-feed-details px-4">
+      <h1 className="flex justify-center text-2xl font-bold text-gray-900 mt-10 dark:text-white">
         {feed.full_name} | ID: {feed.id}
       </h1>
 
-      <div className="grid grid-cols-1 lg:grid-cols-2 gap-4 w-full lg:w-[42rem] xl:w-[45rem]">
-        <DataFeedCardSection
-          key={'dataFeedInfo'}
-          title={dataFeedInfo.title}
-          description={dataFeedInfo.description}
-        >
-          <div className="data-feed-card-content grid grid-cols-2 gap-4">
-            {dataFeedInfo.items.map((item, idx) => (
-              <DataFeedCardContentItem
-                key={idx}
-                label={item.label}
-                value={item.value}
-              />
-            ))}
-          </div>
-        </DataFeedCardSection>
+      <div className="pl-10 grid justify-center grid-cols-[repeat(auto-fill,minmax(350px,1fr))] gap-4 max-w-screen-lg mx-auto">
+        <CoreConfigCard feed={feed} />
 
-        {priceFeedInfo && (
-          <DataFeedCardSection
-            key={'priceFeedInfo'}
-            title={priceFeedInfo.title}
-            description={priceFeedInfo.description}
-          >
-            <div className="data-feed-card-content grid grid-cols-2 gap-4">
-              {priceFeedInfo.items.map((item, idx) => (
-                <DataFeedCardContentItem
-                  key={idx}
-                  label={item.label}
-                  value={item.value}
-                />
-              ))}
-            </div>
-          </DataFeedCardSection>
-        )}
-        {/* <DataFeedCardSection
-          key={`evm-access-info`}
-          title="EVM Access Info"
-          description="To access that from this feed on-chain you can use one of the following approaches:"
-        >
-          <DataFeedCardContentItem
-            label={
-              <a
-                href="/docs/contracts/integration-guide/using-data-feeds/historic-data-feed"
-                target="_blank"
-                rel="noopener noreferrer"
-              >
-                <h5 className="hover:underline">
-                  ProxyCall for direct access:
-                </h5>
-              </a>
-            }
-            value={feedRegistry.directAccess}
-          />
-          <DataFeedCardContentItem
-            label={
-              <a
-                href="/docs/contracts/integration-guide/using-data-feeds/chainlink-proxy"
-                target="_blank"
-                rel="noopener noreferrer"
-              >
-                <h5 className="hover:underline">
-                  Chainlink-style AggregatorProxy:
-                </h5>
-              </a>
-            }
-            value={feedRegistry.aggregatorProxyAddress}
-          />
-          {feedRegistry.chainlinkStyleRegistry && (
-            <div className="data-feed-card-content">
-              <DataFeedCardContentItem
-                label={
-                  <a
-                    href="/docs/contracts/integration-guide/using-data-feeds/feed-registry"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                  >
-                    <h5 className="hover:underline">
-                      Chainlink-style FeedRegistry:
-                    </h5>
-                  </a>
-                }
-                value={feedRegistry.chainlinkStyleRegistry}
-              />
-            </div>
-          )}
-        </DataFeedCardSection> */}
-        {/* <DataFeedCardSection
-          key={`questions`}
-          title="Questions?"
-          description="If you have any questions speak with our team on one of the following platforms:"
-        >
-          <DataFeedCardContentItem label="" value={<QuestionsCardContent />} />
-        </DataFeedCardSection> */}
+        {feed.type === 'price-feed' && <PriceFeedConfigCard feed={feed} />}
+
+        <ConsensusConfigCard feed={feed} />
+        <NetworkAccessCard feedsDeploymentInfo={feedsDeploymentInfo} />
+      </div>
+      <div className="flex lg:justify-center">
+        <OracleConfigCard feed={feed} />
       </div>
     </div>
   );
