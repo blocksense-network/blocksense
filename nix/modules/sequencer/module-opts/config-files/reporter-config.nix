29b45635c542d59f3429608963a96acdaae57c31
Martin Ivanov <xeartybg@gmail.com>
2025-03-28 13:52:16 +0200

    refactor(nix/modules): Rename `reporter-id` to `id`

--- nix/modules/sequencer/module-opts/config-files/reporter-config.nix
+++ nix/modules/sequencer/module-opts/config-files/reporter-config.nix
@@ -9,11 +9,15 @@ in
 {
   mkReporterConfig =
     reporter-opts:
+    let
+      inherit (reporter-opts) reporter-info;
+    in
     dashToUnderscoreRecursive {
-      inherit (reporter-opts.reporter-info) reporter-id interval-time-in-seconds secret-key;
+      inherit (reporter-opts.reporter-info) interval-time-in-seconds secret-key;
 
-      sequencer = reporter-opts.reporter-info.sequencer-url;
-      registry = reporter-opts.reporter-info.registry-url;
+      reporter-id = reporter-info.id;
+      sequencer = reporter-info.sequencer-url;
+      registry = reporter-info.registry-url;
 
       capabilities = builtins.attrValues (
         builtins.mapAttrs (id: data: { inherit id data; }) reporter-opts.api-keys
