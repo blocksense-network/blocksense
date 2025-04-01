f9fd9d99faa953e984bafc25c2fe7ad1c4d984af
Kalin Hristov <kalinsky.hristov@gmail.com>
2025-04-01 17:53:48 +0300

    feat(docs.ws/refactor.ui): Initial commit -> (to be deleted)

---
+++ apps/docs.blocksense.network/components/common/CarouselDemo.tsx
@@ -0,0 +1,33 @@
+import {
+  Carousel,
+  CarouselContent,
+  CarouselItem,
+  CarouselNext,
+  CarouselPrevious,
+} from '../carouselCode/CarouselCode';
+
+export const CarouselDemo = () => {
+  return (
+    <Carousel className="carousel w-full">
+      <CarouselContent className="carousel__content">
+        <CarouselItem className="carousel__item flex justify-center">
+          <div className="flex items-center justify-center w-48 h-48 border border-gray-300">
+            1
+          </div>
+        </CarouselItem>
+        <CarouselItem className="carousel__item flex justify-center">
+          <div className="flex items-center justify-center w-48 h-48 border border-gray-300">
+            2
+          </div>
+        </CarouselItem>
+        <CarouselItem className="carousel__item flex justify-center">
+          <div className="flex items-center justify-center w-48 h-48 border border-gray-300">
+            3
+          </div>
+        </CarouselItem>
+      </CarouselContent>
+      <CarouselPrevious />
+      <CarouselNext />
+    </Carousel>
+  );
+};
