ce95d973ebffd0c1a782745b284fe7b3b4e2a4fb
Emil Ivanichkov <emil.ivanichkov@gmail.com>
2025-04-02 13:25:01 +0200

    feat(DFCG/data-services): Implement `getCryptoProvidersData`
    Co-authored-by: Mila Genova <mila.genova@blocksense.network>

---
+++ apps/data-feeds-config-generator/src/data-services/processors/crypto-providers/data-collection.ts
@@ -0,0 +1,31 @@
+import * as aggregatorFetchers from '../../fetchers/aggregators/index';
+import * as exchangeFetchers from '../../fetchers/exchanges/index';
+
+import { keysOf } from '@blocksense/base-utils/array-iter';
+import { assertNotNull } from '@blocksense/base-utils/assert';
+import { CryptoProviderData } from './types';
+
+export async function getCryptoProvidersData() {
+  const fetcherCategories = {
+    exchanges: exchangeFetchers,
+    aggregators: aggregatorFetchers,
+  };
+
+  const allFetchers = { ...exchangeFetchers, ...aggregatorFetchers };
+  const exchangeAssetsMap: CryptoProviderData[] = await Promise.all(
+    Object.entries(allFetchers).map(async ([name, fetcher]) => {
+      const fetcherData = await new fetcher().fetchAssets();
+      const fetcherName = name.split('AssetsFetcher')[0];
+      return {
+        name: fetcherName,
+        type: assertNotNull(
+          keysOf(fetcherCategories).find(
+            category => name in fetcherCategories[category],
+          ),
+        ),
+        data: fetcherData,
+      };
+    }),
+  );
+  return exchangeAssetsMap;
+}
