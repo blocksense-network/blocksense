ef66b3a2819928d314b119643b88363652733867
Emil Ivanichkov <emil.ivanichkov@gmail.com>
2025-04-02 13:25:01 +0200

    feat(DFCG/data-services): Implement `getCryptoProvidersData`
    Co-authored-by: Mila Genova <mila.genova@blocksense.network>

--- apps/data-feeds-config-generator/scripts/get-providers-data.ts
+++ apps/data-feeds-config-generator/scripts/get-providers-data.ts
@@ -1,39 +1,14 @@
-import { keysOf } from '@blocksense/base-utils/array-iter';
-import { assertNotNull } from '@blocksense/base-utils/assert';
 import { selectDirectory } from '@blocksense/base-utils/fs';
-
-import * as aggregatorFetchers from '../src/data-services/fetchers/aggregators/index';
-import * as exchangeFetchers from '../src/data-services/fetchers/exchanges/index';
-import { ProviderData } from '../src/generation/initial/data-providers';
 import { artifactsDir } from '../src/paths';
+import { getCryptoProvidersData } from '../src/data-services/processors/crypto-providers/data-collection';
 
 async function main() {
-  const fetcherCategories = {
-    exchanges: exchangeFetchers,
-    aggregators: aggregatorFetchers,
-  };
-
-  const allFetchers = { ...exchangeFetchers, ...aggregatorFetchers };
-  const exchangeAssetsMap: ProviderData[] = await Promise.all(
-    Object.entries(allFetchers).map(async ([name, fetcher]) => {
-      const fetcherData = await new fetcher().fetchAssets();
-      const fetcherName = name.split('AssetsFetcher')[0];
-      return {
-        name: fetcherName,
-        type: assertNotNull(
-          keysOf(fetcherCategories).find(
-            category => name in fetcherCategories[category],
-          ),
-        ),
-        data: fetcherData,
-      };
-    }),
-  );
+  const cryptoProvidersData = await getCryptoProvidersData();
 
   const { writeJSON } = selectDirectory(artifactsDir);
   await writeJSON({
     name: 'providers_data',
-    content: exchangeAssetsMap,
+    content: cryptoProvidersData,
   });
 }
 
