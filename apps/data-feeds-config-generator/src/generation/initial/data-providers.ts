16e02659d8cf3fc64e8850a721d0738aa5d852d4
Emil Ivanichkov <emil.ivanichkov@gmail.com>
2025-04-02 11:45:40 +0200

    refactor(DFCG): Migrate crypto providers utils
    Migrate crypto providets utils from `generation/initial/data-providers.ts`
    to `data-services/processors/crypto-providers` as they are not directly
    related with the initial feeds config generation and they can be reused
    in other parts of feeds config generation pipeline.
    
    Co-authored-by: Mila Genova <mila.genova@blocksense.network>

--- apps/data-feeds-config-generator/src/generation/initial/data-providers.ts
+++ apps/data-feeds-config-generator/src/generation/initial/data-providers.ts
@@ -1,25 +1,10 @@
-import { selectDirectory } from '@blocksense/base-utils/fs';
-
-import { keysOf } from '@blocksense/base-utils/array-iter';
-import { equalsCaseInsensitive } from '@blocksense/base-utils/string';
-import {
-  Pair,
-  CryptoPriceFeedsArgs,
-} from '@blocksense/config-types/data-feeds-config';
-
-import { artifactsDir } from '../../paths';
-import { AssetInfo } from '../../data-services/fetchers/exchanges/exchange-assets';
 import { SimplifiedFeed } from './types';
-
-export type ProviderData = {
-  name: string;
-  type: 'exchanges' | 'aggregators';
-  data: AssetInfo[];
-};
+import { getAllProvidersForPair } from '../../data-services/processors/crypto-providers/data-transformers';
+import { CryptoProviderData } from '../../data-services/processors/crypto-providers/types';
 
 export async function addDataProviders(
   dataFeeds: SimplifiedFeed[],
-  providersData: ProviderData[],
+  providersData: CryptoProviderData[],
 ) {
   // Filter out feeds without a quote pair
   const filteredFeeds = filterFeedsWithQuotes(dataFeeds);
@@ -49,107 +34,9 @@ export async function addDataProviders(
   return feedsWithExchangeProviders;
 }
 
-// Function to get all providers for a pair
-export function getAllProvidersForPair(
-  pair: Pair,
-  providersData: ProviderData[],
-): CryptoPriceFeedsArgs {
-  const providers: CryptoPriceFeedsArgs = {};
-
-  const addProvider = (key: string, type: ProviderData['type'], value: any) => {
-    if (value) {
-      providers[type] ??= {};
-      providers[type][key] = value;
-    }
-  };
-
-  providersData.forEach(exchangeData => {
-    addProvider(
-      exchangeData.name,
-      exchangeData.type,
-      getProviderResourcesForPair(pair, exchangeData.data),
-    );
-  });
-
-  return providers;
-}
-
-/**
- * Get provider resources for a given pair.
- *
- * This code takes an array of supported assets by an exchange and reduces it into a single object (`providerInfo`).
- * Each key in the `providerInfo` object corresponds to a key found in the `data` property of the assets.
- * The values are arrays that aggregate all the values associated with that key across all assets.
- * If no data is aggregated (i.e., `providerInfo` has no keys), the function returns `null`.
- * Otherwise, it returns the `providerInfo` object.
- *
- * If `excludePriceInfo` is true, the `price` key is excluded from the aggregation.
- */
-export function getProviderResourcesForPair(
-  pair: Pair,
-  providerAssets: AssetInfo[],
-  excludePriceInfo: boolean = true,
-  includeStableCoins: boolean = true,
-): Record<string, (string | number)[]> | null {
-  const supportedAssets = providerAssets.filter(symbol =>
-    isPairSupportedByCryptoProvider(pair, symbol.pair, includeStableCoins),
-  );
-  const providerInfo = supportedAssets.reduce(
-    (acc, asset) => {
-      keysOf(asset.data).forEach(key => {
-        if (excludePriceInfo && key === 'price') return;
-        const curr = acc[key] ?? [];
-        acc[key] = [...curr, asset.data[key]].sort();
-      });
-      return acc;
-    },
-    {} as Record<string, (string | number)[]>,
-  );
-
-  if (keysOf(providerInfo).length === 0) {
-    return null;
-  }
-
-  return providerInfo;
-}
-
 // Filter feeds that have a quote
 function filterFeedsWithQuotes(feeds: SimplifiedFeed[]): SimplifiedFeed[] {
   return feeds.filter(feed => feed.additional_feed_info.pair.quote);
-}
-
-// Save data to JSON file
-async function saveToFile(data: any, fileName: string) {
-  const { writeJSON } = selectDirectory(artifactsDir);
-  await writeJSON({ content: { data }, name: fileName });
-}
-
-// Placeholder for now; to be extended later
-export const stableCoins = {
-  USD: ['USDT', 'USDC'],
-  EUR: ['EURC', 'EURS', 'EURt'],
-};
-
-// Pair validation logic
-function isPairSupportedByCryptoProvider(
-  pair: Pair,
-  dataProviderPair: Pair,
-  includeStableCoins: boolean = true,
-): boolean {
-  const isBaseCompatible = equalsCaseInsensitive(
-    pair.base,
-    dataProviderPair.base,
-  );
-  const isCompatibleQuote =
-    equalsCaseInsensitive(pair.quote, dataProviderPair.quote) ||
-    (includeStableCoins &&
-      pair.quote in stableCoins &&
-      // Consider stablecoins quotings equivalent to fiat quotings:
-      stableCoins[pair.quote as keyof typeof stableCoins].includes(
-        dataProviderPair.quote,
-      ));
-
-  return isBaseCompatible && isCompatibleQuote;
 }
 
 export function detectOutliers(
