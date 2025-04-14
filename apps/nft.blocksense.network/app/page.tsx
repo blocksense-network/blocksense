7db55a14e257c03fff1a74fcfded1279de386301
Vanya Pavlova <vanya.pavlova@blocksense.network>
2025-04-14 11:06:04 +0300

    feat(nft.blocksense.network): Add CTA

--- apps/nft.blocksense.network/app/page.tsx
+++ apps/nft.blocksense.network/app/page.tsx
@@ -2,13 +2,19 @@ import type { Metadata } from 'next';
 
 import { nftDropConfig } from '../config';
 import { Hero } from 'components/Hero';
+import { CTA } from 'components/CTA';
 
 export const metadata: Metadata = {
   title: nftDropConfig.title,
 };
 
 const NFTDropPage = () => {
-  return <Hero />;
+  return (
+    <>
+      <Hero />
+      <CTA />
+    </>
+  );
 };
 
 export default NFTDropPage;
