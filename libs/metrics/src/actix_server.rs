570a16beae384494c8fb857ef0228e7fe73b0b26
Martin Nikov <mnikov12@gmail.com>
2025-04-16 16:26:18 +0300

    fix(reporter): Fix `prometheus_url`

--- libs/metrics/src/actix_server.rs
+++ libs/metrics/src/actix_server.rs
@@ -22,7 +22,10 @@ pub async fn run_actix_server() -> std::io::Result<()> {
             .service(push_to_buffer)
             .service(pull_buffer)
     })
-    .bind(get_env_var::<String>("PROMETHEUS_URL_SERVER").unwrap_or("0.0.0.0:8080".to_string()))?
+    .bind(
+        get_env_var::<String>("PROMETHEUS_URL_SERVER")
+            .unwrap_or("http://0.0.0.0:9091/metrics/job/reporter".to_string()),
+    )?
     .run()
     .await
 }
@@ -40,7 +43,7 @@ pub async fn handle_prometheus_metrics(
     encoder.encode_utf8(&metric_families, &mut buffer).unwrap();
 
     let _ = client
-        .post(format!("{}{}/push", "http://", url))
+        .post(url.to_string())
         .body(buffer.to_string())
         .send()
         .await?;
