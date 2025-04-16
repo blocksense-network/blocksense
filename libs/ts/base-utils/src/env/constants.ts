6050913c1a5f6dd3e70bfd785814fa4ef12af1d9
Emil Ivanichkov <emil.ivanichkov@gmail.com>
2025-04-17 01:56:07 +0300

    feat(base-utils/env/constants): Add `processComposeLogsDir` & `processComposeLogsFiles`

--- libs/ts/base-utils/src/env/constants.ts
+++ libs/ts/base-utils/src/env/constants.ts
@@ -10,3 +10,23 @@ export const rootDir = getEnvString('GIT_ROOT');
  * The root configuration directory.
  */
 export const configDir = join(rootDir, 'config');
+
+/**
+ * The root directory for the Process Compose logs.
+ */
+export const processComposeLogsDir = join(rootDir, 'logs/blocksense');
+
+export const processComposeLogsFiles = {
+  ['aggregate-consensus-reader']: join(
+    processComposeLogsDir,
+    'aggregate-consensus-reader.log.log',
+  ),
+  ['anvil-ethereum-sepolia']: join(
+    processComposeLogsDir,
+    'anvil-ethereum-sepolia.log.log',
+  ),
+  ['anvil-ink-sepolia']: join(processComposeLogsDir, 'anvil-ink-sepolia.log'),
+  ['blockchain-reader']: join(processComposeLogsDir, 'blockchain-reader.log'),
+  ['reporter-a']: join(processComposeLogsDir, 'reporter-a.log'),
+  ['sequencer']: join(processComposeLogsDir, 'sequencer.log'),
+};
