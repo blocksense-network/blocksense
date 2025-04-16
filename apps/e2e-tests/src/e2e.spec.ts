1843953e6da2c423f56815dc42ea557003a16d26
Emil Ivanichkov <emil.ivanichkov@gmail.com>
2025-04-16 23:49:56 +0300

    test(e2e-tests): Add tests to check state ot processes

--- apps/e2e-tests/src/e2e.spec.ts
+++ apps/e2e-tests/src/e2e.spec.ts
@@ -1,22 +1,79 @@
-import { afterAll, beforeAll, describe, test } from 'vitest';
-import { $ } from 'execa';
+import { afterAll, beforeAll, describe, expect, test } from 'vitest';
 
-import { logProcessComposeInfo } from './utils/logs';
+import { sleep } from '@blocksense/base-utils/async';
+
+import {
+  parseProcessesStatus,
+  processComposeOrchestration,
+} from './utils/process-compose';
 
 describe.sequential('E2E Tests with process-compose', async () => {
+  const expectedProcessesStatus = {
+    'aggregate-consensus-reader': {
+      status: 'Running',
+      exit_code: 0,
+    },
+    'anvil-impersonate-and-fund-ethereum-sepolia': {
+      status: 'Completed',
+      exit_code: 0,
+    },
+    'anvil-impersonate-and-fund-ink-sepolia': {
+      status: 'Completed',
+      exit_code: 0,
+    },
+    'anvil-ethereum-sepolia': {
+      status: 'Running',
+      exit_code: 0,
+    },
+    'anvil-ink-sepolia': {
+      status: 'Running',
+      exit_code: 0,
+    },
+    'blockchain-reader': {
+      status: 'Running',
+      exit_code: 0,
+    },
+    'blocksense-reporter-a': {
+      status: 'Running',
+      exit_code: 0,
+    },
+    'blocksense-sequencer': {
+      status: 'Running',
+      exit_code: 0,
+    },
+    kafka: {
+      status: 'Running',
+      exit_code: 0,
+    },
+  };
+
   beforeAll(async () => {
     // Start the process-compose
-    logProcessComposeInfo('Starting');
-    await $`process-compose up -D`;
+    await processComposeOrchestration('start');
   });
 
   afterAll(async () => {
     // Stop the process-compose
-    logProcessComposeInfo('Stopping');
-    await $`process-compose down`;
+    await processComposeOrchestration('stop');
   });
-  test('should start the process-compose', async () => {
-    const { stdout } = await $`process-compose info`;
-    console.log(stdout);
+
+  test('Test processes state shortly after start', async () => {
+    // Wait for the processes to be up and running
+    await sleep(5000);
+
+    // Get the processes status
+    const processes = await parseProcessesStatus();
+
+    expect(processes).toEqual(expectedProcessesStatus);
+  });
+
+  test('Test processes state after 5 mins', async () => {
+    // Wait for the processes to work for 5 minutes
+    await sleep(2 * 60 * 1000);
+
+    // Get the processes status
+    const processes = await parseProcessesStatus();
+
+    expect(processes).toEqual(expectedProcessesStatus);
   });
 });
