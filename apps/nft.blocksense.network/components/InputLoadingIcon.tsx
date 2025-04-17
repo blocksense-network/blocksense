752bc823ac70a69a6c850b8d545dabc557de73f1
Radoslav Dimchev <radoslav.dimchev@blocksense.network>
2025-04-17 12:26:21 +0300

    feat(nft): Add icons for the `Form` section

---
+++ apps/nft.blocksense.network/components/InputLoadingIcon.tsx
@@ -0,0 +1,16 @@
+export const InputLoadingIcon = () => {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width="24"
+      height="24"
+      viewBox="0 0 24 24"
+      fill="none"
+      className="input-loading-icon"
+    >
+      <circle cx="4" cy="12" r="2" fill="var(--gray-medium)" />
+      <circle cx="12" cy="12" r="2" fill="var(--gray-medium)" />
+      <circle cx="20" cy="12" r="2" fill="var(--gray-medium)" />
+    </svg>
+  );
+};
