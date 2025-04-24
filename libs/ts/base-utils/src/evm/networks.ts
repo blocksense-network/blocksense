b0e54c5f22d466ef7ab1ce2d07140cc8ae9519b9
Kristin Kirkov <kristin.kirkov@metacraft-labs.com>
2025-04-24 11:24:50 +0300

    feat(base-utils): Add soneium-mainnet to base-utils

--- libs/ts/base-utils/src/evm/networks.ts
+++ libs/ts/base-utils/src/evm/networks.ts
@@ -103,6 +103,7 @@ const networks = [
   'scroll-sepolia',
   'shape-sepolia',
   'somnia-testnet',
+  'soneium-mainnet',
   'songbird-coston',
   'sonic-mainnet',
   'sonic-blaze',
@@ -133,8 +134,9 @@ const chainIds = [
   1285, 59144, 59141, 4202, 994873017, 1952959480, 169, 3441006, 5000, 5003,
   1750, 59902, 6342, 31611, 10143, 2818, 2810, 8801, 5851, 10, 11155420, 5611,
   7849306, 50002, 98866, 98867, 137, 80002, 1101, 2442, 57000, 200018, 31,
-  534352, 534351, 11011, 50312, 16, 146, 57054, 1660990954, 5330, 53302, 1924,
-  2390, 167000, 167009, 5678, 842, 41, 130, 1301, 4801, 1417429182, 324, 300,
+  534352, 534351, 11011, 50312, 16, 1868, 146, 57054, 1660990954, 5330, 53302,
+  1924, 2390, 167000, 167009, 5678, 842, 41, 130, 1301, 4801, 1417429182, 324,
+  300,
 ] as const;
 
 export const networkName = S.Literal(...networks);
@@ -1104,6 +1106,16 @@ export const networkMetadata = {
       apiUrl: 'https://somnia-testnet.socialscan.io/api',
     },
     currency: Currency.STT,
+  },
+  'soneium-mainnet': {
+    chainId: 1868,
+    isTestnet: false,
+    explorer: {
+      type: 'blockscout',
+      webUrl: 'https://soneium.blockscout.com',
+      apiUrl: 'https://soneium.blockscout.com/api',
+    },
+    currency: Currency.ETH,
   },
   'songbird-coston': {
     chainId: 16,
