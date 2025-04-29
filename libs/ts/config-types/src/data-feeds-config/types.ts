958f1ff001e748ad0bbfc6d98d7c49ef6461a0f8
Petar Kirov <petar.p.kirov@gmail.com>
2025-04-29 19:45:02 +0300

    wip

--- libs/ts/config-types/src/data-feeds-config/types.ts
+++ libs/ts/config-types/src/data-feeds-config/types.ts
@@ -149,7 +149,7 @@ export type MarketHours = typeof MarketHoursSchema.Type;
 
 export const NewFeedSchema = S.mutable(
   S.Struct({
-    id: S.Number,
+    id: S.BigInt,
     full_name: S.String,
     description: S.String,
 
