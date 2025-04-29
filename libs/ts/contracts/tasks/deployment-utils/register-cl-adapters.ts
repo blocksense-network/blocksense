a7f8e73f3549b3652fd2018b33c011d5daaa74a3
Petar Kirov <petar.p.kirov@gmail.com>
2025-04-29 19:45:02 +0300

    wip

--- libs/ts/contracts/tasks/deployment-utils/register-cl-adapters.ts
+++ libs/ts/contracts/tasks/deployment-utils/register-cl-adapters.ts
@@ -5,6 +5,7 @@ import {
   SafeTransactionDataPartial,
 } from '@safe-global/safe-core-sdk-types';
 import Safe from '@safe-global/protocol-kit';
+import { entriesOf } from '@blocksense/base-utils/array-iter';
 
 import {
   CLAggregatorAdapterData,
@@ -42,12 +43,11 @@ export async function registerCLAdapters({
   // Split into batches of 100
   const BATCH_LENGTH = 100;
   const batches: Array<Array<CLAggregatorAdapterData>> = [];
-  const aggregatorData = deployData.CLAggregatorAdapter.filter(d => d.base);
   const filteredData = [];
 
-  for (const data of aggregatorData) {
+  for (const [description, data] of entriesOf(deployData.CLAggregatorAdapter)) {
     if (!data.base || !data.quote) {
-      console.log(` -> Feed '${data.description}' has no base or quote`, '\n');
+      console.log(` -> Feed '${description}' has no base or quote`, '\n');
       continue;
     }
 
@@ -60,7 +60,7 @@ export async function registerCLAdapters({
       filteredData.push(data);
     } else {
       console.log(
-        ` -> Feed '${data.description}' already registered`,
+        ` -> Feed '${description}' already registered`,
         {
           base: data.base,
           quote: data.quote,
