0e6eaabdf501065c8add509555f4b4aefd657ef1
Petar Kirov <petar.p.kirov@gmail.com>
2025-05-03 15:14:58 +0300

    refactor(contracts/tasks): Convert `init-chain` HH task to regular function

--- libs/ts/contracts/tasks/multichain-deploy.ts
+++ libs/ts/contracts/tasks/multichain-deploy.ts
@@ -17,6 +17,7 @@ import { getOptionalEnvString } from '@blocksense/base-utils/env';
 import { DeploymentConfigV2 } from '@blocksense/config-types/evm-contracts-deployment';
 import { predictAddress } from './utils';
 import { readConfig, writeEvmDeployment } from '@blocksense/config-types';
+import { initChain } from './deployment-utils/init-chain';
 
 task('deploy', 'Deploy contracts')
   .addParam('networks', 'Network to deploy to')
@@ -27,7 +28,7 @@ task('deploy', 'Deploy contracts')
       if (!isNetworkName(network)) {
         throw new Error(`Invalid network: ${network}`);
       }
-      configs.push(await run('init-chain', { networkName: network }));
+      configs.push(await initChain(ethers, network));
     }
 
     const { feeds } = await readConfig('feeds_config_v2');
