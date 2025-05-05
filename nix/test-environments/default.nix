53c01f9dd31900fd45e0babfb4a46c6943e38f6e
Simeon Armenchev <monyarm@gmail.com>
2025-05-05 15:20:36 +0300

    feat(test-environments): Improve NixOS Integration test

--- nix/test-environments/default.nix
+++ nix/test-environments/default.nix
@@ -117,14 +117,34 @@
               self.nixosModules.blocksense-systemd
               ./example-setup-01.nix
               self.nixosModules.example-setup-vm
+              {
+                virtualisation.memorySize = 16 * 1024;
+                virtualisation.cores = 4;
+                networking.nameservers = [ "8.8.8.8" ];
+              }
             ];
           };
           testScript = ''
             machine.start();
-            machine.wait_for_unit("blocksense-sequencer.service");
-            machine.wait_for_unit("blocksense-reporter-a.service");
+            machine.wait_for_unit("network-online.target");
+
             machine.wait_for_unit("blocksense-anvil-ethereum-sepolia.service");
             machine.wait_for_unit("blocksense-anvil-ink-sepolia.service");
+            machine.wait_for_unit("blocksense-sequencer.service");
+            machine.wait_for_unit("blocksense-reporter-a.service");
+
+            machine.wait_for_open_port(8546);
+            machine.wait_for_open_port(8547);
+            machine.wait_for_open_port(5553);
+
+            # Using UpgradeableProxy contract: Reading last update for feed with id 0
+            machine.succeed("cast call 0xee5a4826068c5326a7f06fd6c7cbf816f096846c --data 0x80000000 --rpc-url http://127.0.0.1:8546 | cut -c1-50 | cast to-dec");
+            #Using ChainlinkProxy contract:Reading the latest answer from a ChainlinkProxy contract
+            machine.succeed("cast call 0x9fAb38E38d526c6ba82879c6bDe1c4Fd73378f17 \"latestAnswer()\" --rpc-url http://127.0.0.1:8546 | cast to-dec");
+            #Using UpgradeableProxyADFS contract: Reading the last update for feed with id 0
+            machine.succeed("cast call 0xADF5aad6faA8f2bFD388D38434Fc45625Ebd9d3b --data 0x8200000000000000000000000000000000  --rpc-url http://127.0.0.1:8547 | cut -c1-50 | cast to-dec");
+            #Using CLAggregatorAdapter contract: Reading the latest answer from a CLAggregatorAdapter contract
+            machine.succeed("cast call 0xcBD6FC059bEDc859d43994F5C221d96F9eD5340f \"latestAnswer()\" --rpc-url http://127.0.0.1:8547 | cast to-dec");
           '';
         };
       };
