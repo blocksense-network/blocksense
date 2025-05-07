4e940990e133a9571c17759f52ed0e9d1dbebf56
Martin Ivanov <xeartybg@gmail.com>
2025-05-07 15:07:56 +0300

    nix(build): Use cargo target dir for process-compose blocksense backend

--- nix/shells/pkg-sets/rust.nix
+++ nix/shells/pkg-sets/rust.nix
@@ -2,6 +2,8 @@
   pkgs,
   self',
   config,
+  lib,
+  inputs',
   ...
 }:
 {
@@ -10,6 +12,7 @@
     REPORTER_CONFIG_DIR = config.devenv.root + "/apps/reporter";
     FEEDS_CONFIG_DIR = config.devenv.root + "/config";
     REPORTER_SECRET_KEY_FILE_PATH = config.devenv.root + "/nix/test-environments/test-keys";
+    LD_LIBRARY_PATH = lib.makeLibraryPath self'.legacyPackages.commonLibDeps;
   };
 
   enterShell = ''
@@ -24,7 +27,7 @@
 
   packages = self'.packages.blocksense-rs.buildInputs ++ [
     self'.legacyPackages.cargoWrapped
-    self'.legacyPackages.spinWrapped
+    inputs'.nixpkgs-unstable.legacyPackages.fermyon-spin
     self'.legacyPackages.rustToolchain
     pkgs.cargo-tarpaulin
   ];
