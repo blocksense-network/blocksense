2399aab9f6c96da764019b30b26f8374e8ac7351
Petar Kirov <petar.p.kirov@gmail.com>
2025-05-10 20:16:49 +0300

    improve(base-utils,config-types): Add annotations to some schemas

--- libs/ts/config-types/src/evm-contracts-deployment/index.ts
+++ libs/ts/config-types/src/evm-contracts-deployment/index.ts
@@ -19,6 +19,8 @@ export const CLAggregatorAdapterDataSchema = S.Struct({
   feedId: S.BigInt, // bigint encoded as string in JSON
   base: S.NullOr(ethereumAddress),
   quote: S.NullOr(ethereumAddress),
+}).annotations({
+  identifier: 'CLAggregatorAdapterData',
 });
 
 export type CLAggregatorAdapterData = typeof CLAggregatorAdapterDataSchema.Type;
@@ -58,7 +60,7 @@ const ContractsConfigSchemaV2 = S.mutable(
     SequencerMultisig: ethereumAddress,
     AdminMultisig: ethereumAddress,
   }),
-);
+).annotations({ identifier: 'ContractsConfigV2' });
 
 export type ContractsConfigV2 = typeof ContractsConfigSchemaV2.Type;
 
@@ -80,6 +82,8 @@ export const DeploymentConfigSchemaV2 = S.mutable(
     chainId: chainId,
     contracts: ContractsConfigSchemaV2,
   }),
-);
+).annotations({
+  identifier: 'DeploymentConfigV2',
+});
 
 export type DeploymentConfigV2 = typeof DeploymentConfigSchemaV2.Type;
