a53683cf2553d953175cebfbba949bf419ffaf7f
Radoslav Dimchev <radoslav.dimchev@blocksense.network>
2025-05-11 22:00:45 +0300

    feat(nft): Add mint transaction url to `SuccessForm`

--- apps/nft.blocksense.network/components/SuccessForm.tsx
+++ apps/nft.blocksense.network/components/SuccessForm.tsx
@@ -1,8 +1,13 @@
 import Image from 'next/image';
 
 import successFormBlocksenseLogo from '/public/icons/success-form-blocksense-logo.svg';
+import exploreIcon from '/public/icons/explore.svg';
 
-export const SuccessForm = () => {
+type SuccessFormProps = {
+  mintTransactionUrl: string;
+};
+
+export const SuccessForm = ({ mintTransactionUrl }: SuccessFormProps) => {
   return (
     <form className="success-form w-full mx-auto flex flex-col items-center justify-center md:gap-8 gap-6 md:p-8 p-6 bg-[var(--gray-dark)] md:rounded-3xl rounded-2xl text-center">
       <Image
@@ -10,7 +15,7 @@ export const SuccessForm = () => {
         alt="Success Form Blocksense Logo"
         className="success-form__blocksense-logo"
       />
-      <article className="success-form__article flex flex-col gap-4">
+      <article className="success-form__article flex flex-col md:gap-6 gap-5 justify-center items-center">
         <h3 className="success-form__title md:text-2xl text-base">
           Thank you for completing all the steps.
         </h3>
@@ -19,6 +24,21 @@ export const SuccessForm = () => {
           Glad to have you onboard. You are now part of
           <br /> the permissionless oracle revolution.
         </p>
+        <a
+          href={mintTransactionUrl}
+          target="_blank"
+          rel="noopener noreferrer"
+          className="w-fit flex justify-center items-center gap-1 cursor-pointer"
+        >
+          <Image
+            src={exploreIcon}
+            alt="Explore"
+            className="success-form__explore-icon"
+          />
+          <p className="text-[var(--white)] underline md:text-base text-sm leading-[1.225rem]">
+            Explore your minting transaction
+          </p>
+        </a>
       </article>
     </form>
   );
