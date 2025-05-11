f6384f0a58ac9e451ecf2f06650ae726c0a2a7fd
Radoslav Dimchev <radoslav.dimchev@blocksense.network>
2025-05-11 22:02:04 +0300

    feat(nft): Implement context & update states for `MintForm`

---
+++ apps/nft.blocksense.network/app/contexts/MintFormContext.tsx
@@ -0,0 +1,102 @@
+import React, {
+  createContext,
+  Dispatch,
+  ReactNode,
+  SetStateAction,
+  useContext,
+  useState,
+} from 'react';
+
+type VerificationStatus = {
+  type: 'error' | 'success' | 'loading' | 'none';
+  message: string;
+};
+
+type MintFormContextType = {
+  xHandle: string;
+  setXHandle: Dispatch<SetStateAction<string>>;
+  xStatus: VerificationStatus;
+  setXStatus: Dispatch<SetStateAction<VerificationStatus>>;
+  xUserId: string | null;
+  setXUserId: Dispatch<SetStateAction<string | null>>;
+
+  discord: string;
+  setDiscord: Dispatch<SetStateAction<string>>;
+  discordStatus: VerificationStatus;
+  setDiscordStatus: Dispatch<SetStateAction<VerificationStatus>>;
+
+  retweetStatus: VerificationStatus;
+  setRetweetStatus: Dispatch<SetStateAction<VerificationStatus>>;
+  retweetCode: string;
+  setRetweetCode: Dispatch<SetStateAction<string>>;
+
+  alertMessage: string;
+  setAlertMessage: Dispatch<SetStateAction<string>>;
+  mintLoading: boolean;
+  setMintLoading: Dispatch<SetStateAction<boolean>>;
+};
+
+const MintFormContext = createContext<MintFormContextType>(
+  {} as MintFormContextType,
+);
+
+export const MintFormProvider = ({ children }: { children: ReactNode }) => {
+  const [xHandle, setXHandle] = useState('');
+  const [xStatus, setXStatus] = useState<VerificationStatus>({
+    type: 'none',
+    message: '',
+  });
+  const [xUserId, setXUserId] = useState<string | null>(null);
+
+  const [discord, setDiscord] = useState('');
+  const [discordStatus, setDiscordStatus] = useState<VerificationStatus>({
+    type: 'none',
+    message: '',
+  });
+
+  const [retweetStatus, setRetweetStatus] = useState<VerificationStatus>({
+    type: 'none',
+    message: '',
+  });
+  const [retweetCode, setRetweetCode] = useState('');
+
+  const [alertMessage, setAlertMessage] = useState('');
+  const [mintLoading, setMintLoading] = useState(false);
+
+  return (
+    <MintFormContext.Provider
+      value={{
+        xHandle,
+        setXHandle,
+        xUserId,
+        setXUserId,
+        xStatus,
+        setXStatus,
+        discord,
+        setDiscord,
+        discordStatus,
+        setDiscordStatus,
+        retweetStatus,
+        setRetweetStatus,
+        retweetCode,
+        setRetweetCode,
+        alertMessage,
+        setAlertMessage,
+        mintLoading,
+        setMintLoading,
+      }}
+    >
+      {children}
+    </MintFormContext.Provider>
+  );
+};
+
+export const useMintFormContext = () => {
+  const context = useContext(MintFormContext);
+  if (!context) {
+    throw new Error(
+      'useMintFormContext must be used within a MintFormProvider',
+    );
+  }
+  return context;
+};
