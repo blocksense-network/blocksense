c8fb6b39e6a66002e73d63d1719ef296165d04fb
Emil Ivanichkov <emil.ivanichkov@gmail.com>
2025-05-11 17:15:58 +0300

    feat(base-utils/async): Add `loopWhile` function
    Also add tests

--- libs/ts/base-utils/src/async.ts
+++ libs/ts/base-utils/src/async.ts
@@ -5,6 +5,47 @@
  */
 
 /**
+ * Pauses execution for a specified duration.
+ * @param ms - The number of milliseconds to sleep.
+ * @returns A promise that resolves after the specified duration.
+ */
+export function sleep(ms: number) {
+  return new Promise(resolve => setTimeout(resolve, ms));
+}
+
+/**
+ * Executes a function repeatedly while a given condition is true.
+ *
+ * @param cond A function that takes the result of the repeated function as an argument and returns a boolean value.
+ * @param f The function to execute repeatedly.
+ * @param time The amount of time to wait between each execution of the function (in ms).
+ * @param maxAttempts Optional maximum number of attempts before throwing an error. Default is 100.
+ * @return The final result of the repeated function.
+ */
+export async function loopWhile<T>(
+  cond: (r: T) => boolean,
+  f: () => Promise<T>,
+  time: number,
+  maxAttempts: number = 100,
+): Promise<T> {
+  let attempts = 0;
+  let result = await f();
+  attempts++;
+
+  while (cond(result)) {
+    if (maxAttempts !== undefined && attempts >= maxAttempts) {
+      throw new Error(`loopWhile: Reached maxAttempts (${maxAttempts})`);
+    }
+
+    await sleep(time);
+    result = await f();
+    attempts++;
+  }
+
+  return result;
+}
+
+/**
  * Filters an array based on an asynchronous predicate.
  *
  * @template T The type of the elements in the input array.
