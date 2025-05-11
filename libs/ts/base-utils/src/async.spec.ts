14c41e1aa7d0b315f12ca9633544087718fd02f8
Emil Ivanichkov <emil.ivanichkov@gmail.com>
2025-05-11 17:15:58 +0300

    feat(base-utils/async): Add `loopWhile` function
    Also add tests

--- libs/ts/base-utils/src/async.spec.ts
+++ libs/ts/base-utils/src/async.spec.ts
@@ -1,6 +1,6 @@
 import { describe, expect, it } from 'vitest';
 
-import { filterAsync } from './async';
+import { filterAsync, loopWhile } from './async';
 
 describe('filterAsync', () => {
   it('should filter an array based on an async predicate', async () => {
@@ -19,5 +19,35 @@ describe('filterAsync', () => {
     const result = await filterAsync(array, asyncPredicate);
 
     expect(result).toEqual([]);
+  });
+});
+
+describe('loopWhile', () => {
+  it('executes the function while the condition is true', async () => {
+    let counter = 0;
+    const cond = (r: number) => r < 3;
+    const f = () => Promise.resolve(counter++);
+    const result = await loopWhile(cond, f, 100);
+    expect(result).toEqual(3);
+  });
+
+  it('waits the specified amount of time between each execution of the function', async () => {
+    let counter = 0;
+    const cond = (r: number) => r < 3;
+    const f = () => Promise.resolve(counter++);
+    const startTime = Date.now();
+    await loopWhile(cond, f, 100);
+    const endTime = Date.now();
+    expect(endTime - startTime).toBeGreaterThanOrEqual(300);
+  });
+
+  it('throws an error if maxAttempts is reached', async () => {
+    let counter = 0;
+    const cond = () => true; // always returns true so loop never naturally exits
+    const f = () => Promise.resolve(counter++);
+
+    await expect(loopWhile(cond, f, 50, 3)).rejects.toThrow(
+      'loopWhile: Reached maxAttempts (3)',
+    );
   });
 });
