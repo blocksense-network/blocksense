301ee7d5697b161fba36f1e6053b58b743a58617
Kristin Kirkov <kristin.kirkov@metacraft-labs.com>
2025-05-13 17:05:40 +0300

    feat(base-utils): Move startPrometheusServer function

--- libs/ts/onchain-interactions/src/scripts/balance-script.ts
+++ libs/ts/onchain-interactions/src/scripts/balance-script.ts
@@ -2,6 +2,7 @@ import Web3 from 'web3';
 import yargs from 'yargs';
 import { hideBin } from 'yargs/helpers';
 import chalk from 'chalk';
+import client from 'prom-client';
 import {
   getOptionalRpcUrl,
   networkMetadata,
@@ -9,29 +10,13 @@ import {
 } from '@blocksense/base-utils/evm';
 import { deployedNetworks } from '../types';
 import { getEnvStringNotAssert } from '@blocksense/base-utils/env';
-import client from 'prom-client';
-import express from 'express';
+import { startPrometheusServer } from '@blocksense/base-utils/prometheus';
 
 const balanceGauge = new client.Gauge({
   name: 'eth_account_balance',
   help: 'Ethereum account balance in Ether',
   labelNames: ['chainId', 'address'],
 });
-
-const startPrometheusServer = (host: string, port: number): void => {
-  const app = express();
-  app.get('/metrics', async (_req, res) => {
-    res.set('Content-Type', client.register.contentType);
-    res.end(await client.register.metrics());
-  });
-  app.listen(port, host, () => {
-    console.log(
-      chalk.blue(
-        `Prometheus metrics exposed at http://${host}:${port}/metrics`,
-      ),
-    );
-  });
-};
 
 const main = async (): Promise<void> => {
   const sequencerAddress = getEnvStringNotAssert('SEQUENCER_ADDRESS');
