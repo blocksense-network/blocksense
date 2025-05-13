ca581b7cd0b48750cd61d079232c1c1781892ac8
Petar Kirov <petar.p.kirov@gmail.com>
2025-05-13 15:09:54 +0300

    wip6

--- libs/ts/base-utils/src/env/functions.spec.ts
+++ libs/ts/base-utils/src/env/functions.spec.ts
@@ -3,19 +3,18 @@ import { describe, expect, test } from 'vitest';
 import { Schema as S } from 'effect';
 
 import {
-  asEnvSchema,
-  DeploymentEnvSchema,
+  asVarSchema,
   EnvSchema,
   getEnvString,
-  parseDeploymentEnvConfig,
   parseEnv,
   parseEnvConfig,
-  parseNetworkEnvConfig,
   parseOptionalEnv,
   parseRequiredEnv,
+  VarSchema,
 } from './functions';
-import { ethereumAddress, NetworkName, networkName } from '../evm';
+import { NetworkName, networkName } from '../evm';
 import { fromCommaSeparatedString } from '../schemas';
+import { kebabToSnakeCase } from '../string';
 
 describe('env', () => {
   describe('getEnvString', () => {
@@ -122,54 +121,66 @@ describe('env', () => {
       );
       expect(parsed).toEqual(['ethereum-mainnet', 'arbitrum-sepolia']);
     });
+
+    describe('parsePerNetworkEnv', () => {
+      const parsePerNetworkEnv = <T, S extends string>(
+        varName: string,
+        networkName: NetworkName,
+        schema: VarSchema<T, S>,
+        env: NodeJS.ProcessEnv = process.env,
+      ): T =>
+        parseRequiredEnv(
+          kebabToSnakeCase(`${varName}_${networkName}`),
+          schema,
+          env,
+        );
+
+      test('should parse environment variable for a specific network', () => {
+        expect(
+          parsePerNetworkEnv('HELLO', 'ethereum-mainnet', S.String, {
+            HELLO_ETHEREUM_MAINNET: 'world',
+          }),
+        ).toEqual('world');
+
+        expect(
+          parsePerNetworkEnv('hello', 'ethereum-mainnet', S.String, {
+            HELLO_ETHEREUM_MAINNET: 'world',
+          }),
+        ).toEqual('world');
+
+        expect(
+          parsePerNetworkEnv('HELLO', 'arbitrum-sepolia', S.String, {
+            HELLO_ARBITRUM_SEPOLIA: 'world',
+          }),
+        ).toEqual('world');
+
+        expect(
+          parsePerNetworkEnv('HELLO', 'arbitrum-sepolia', S.String, {
+            HELLO_ARBITRUM_SEPOLIA: 'arbitrum',
+            HELLO_ETHEREUM_MAINNET: 'ethereum',
+          }),
+        ).toEqual('arbitrum');
+
+        expect(
+          parsePerNetworkEnv('HELLO', 'ethereum-mainnet', S.String, {
+            HELLO_ARBITRUM_SEPOLIA: 'arbitrum',
+            HELLO_ETHEREUM_MAINNET: 'ethereum',
+          }),
+        ).toEqual('ethereum');
+      });
+
+      test('should throw an error if the variable is not set', () => {
+        expect(() =>
+          parsePerNetworkEnv('HELLO', 'ethereum-mainnet', S.String),
+        ).toThrowError("Env variable 'HELLO_ETHEREUM_MAINNET' is missing.");
+      });
+    });
   });
-
-  // describe('parsePerNetworkEnv', () => {
-  //   test('should parse environment variable for a specific network', () => {
-  //     expect(
-  //       parsePerNetworkEnv('HELLO', 'ethereum-mainnet', S.String, {
-  //         HELLO_ETHEREUM_MAINNET: 'world',
-  //       }),
-  //     ).toEqual('world');
-
-  //     expect(
-  //       parsePerNetworkEnv('hello', 'ethereum-mainnet', S.String, {
-  //         HELLO_ETHEREUM_MAINNET: 'world',
-  //       }),
-  //     ).toEqual('world');
-
-  //     expect(
-  //       parsePerNetworkEnv('HELLO', 'arbitrum-sepolia', S.String, {
-  //         HELLO_ARBITRUM_SEPOLIA: 'world',
-  //       }),
-  //     ).toEqual('world');
-
-  //     expect(
-  //       parsePerNetworkEnv('HELLO', 'arbitrum-sepolia', S.String, {
-  //         HELLO_ARBITRUM_SEPOLIA: 'arbitrum',
-  //         HELLO_ETHEREUM_MAINNET: 'ethereum',
-  //       }),
-  //     ).toEqual('arbitrum');
-
-  //     expect(
-  //       parsePerNetworkEnv('HELLO', 'ethereum-mainnet', S.String, {
-  //         HELLO_ARBITRUM_SEPOLIA: 'arbitrum',
-  //         HELLO_ETHEREUM_MAINNET: 'ethereum',
-  //       }),
-  //     ).toEqual('ethereum');
-  //   });
-
-  //   test('should throw an error if the variable is not set', () => {
-  //     expect(() =>
-  //       parsePerNetworkEnv('HELLO', 'ethereum-mainnet', S.String),
-  //     ).toThrowError("Env variable 'HELLO_ETHEREUM_MAINNET' is missing.");
-  //   });
-  // });
 
   describe('parseEnvConfig', () => {
     const testSchema1 = {
-      NAME: asEnvSchema(S.NonEmptyTrimmedString),
-      AMOUNT: asEnvSchema(S.NumberFromString),
+      NAME: asVarSchema(S.NonEmptyTrimmedString),
+      AMOUNT: asVarSchema(S.NumberFromString),
     } satisfies EnvSchema;
 
     test('should parse environment variables correctly', () => {
@@ -209,14 +220,6 @@ describe('env', () => {
       }
     });
 
-    // test('should throw an error for missing required variables', () => {
-    //   expect(() =>
-    //     parseEnvConfig(testSchema1, '', {
-    //       NAME: 'Alice',
-    //     }),
-    //   ).toThrowError("Env variable 'AMOUNT' is missing.");
-    // });
-
     test('should throw an error for invalid variable types', () => {
       expect(() =>
         parseEnvConfig(testSchema1, '', {
@@ -245,7 +248,7 @@ describe('env', () => {
     });
 
     const testSchema2 = {
-      MAIN_NETWORK: asEnvSchema(networkName),
+      MAIN_NETWORK: asVarSchema(networkName),
       EXTRA_NETWORKS: fromCommaSeparatedString(networkName),
       BLOCK_NUMBER: S.BigInt,
       RPC_URL: S.URL,
@@ -290,129 +293,6 @@ describe('env', () => {
         ],
         RPC_URL: new URL('https://example.com'),
         BLOCK_NUMBER: 2n ** 128n,
-      });
-    });
-  });
-
-  describe('parseNetworkEnvConfig', () => {
-    const testSchema = {
-      RPC_URL: S.URL,
-      THRESHOLD: S.NumberFromString,
-      BLOCK_NUMBER: S.BigInt,
-      FEED_IDS: S.Union(S.Literal('all'), fromCommaSeparatedString(S.BigInt)),
-    } satisfies EnvSchema;
-
-    test('should parse environment variables for a specific network', () => {
-      const parsed = parseNetworkEnvConfig(testSchema, 'ethereum-mainnet', {
-        RPC_URL_ETHEREUM_MAINNET: 'https://example.com',
-        THRESHOLD_ETHEREUM_MAINNET: '3',
-        BLOCK_NUMBER_ETHEREUM_MAINNET: `${2n ** 128n}`,
-        FEED_IDS_ETHEREUM_MAINNET: '1,2,3',
-      });
-
-      expect(parsed).toEqual({
-        RPC_URL: new URL('https://example.com'),
-        THRESHOLD: 3,
-        BLOCK_NUMBER: 2n ** 128n,
-        FEED_IDS: [1n, 2n, 3n],
-      });
-    });
-  });
-
-  describe('parseDeploymentEnvConfig', () => {
-    const deploymentTestSchema = {
-      global: {
-        NETWORKS: fromCommaSeparatedString(networkName),
-        RETRIES: S.NumberFromString,
-        GAS_LIMIT: S.NumberFromString,
-      },
-      perNetworkKind: {
-        GAS_LIMIT: S.NumberFromString,
-        USE_HW_WALLET: asEnvSchema(S.BooleanFromString),
-        DEPLOYER: ethereumAddress,
-        THRESHOLD: S.NumberFromString,
-      },
-      perNetworkName: {
-        GAS_LIMIT: S.NumberFromString,
-        USE_HW_WALLET: asEnvSchema(S.BooleanFromString),
-        DEPLOYER: ethereumAddress,
-        THRESHOLD: S.NumberFromString,
-      },
-    } satisfies DeploymentEnvSchema;
-
-    const deploymentTestEnv = {
-      NETWORKS:
-        'local, ethereum-sepolia, ethereum-mainnet, arbitrum-sepolia, arbitrum-mainnet',
-      RETRIES: '3',
-
-      GAS_LIMIT: `${10e9}`,
-      GAS_LIMIT_LOCAL: `${15e9}`,
-      GAS_LIMIT_MAINNET: `${10e9}`,
-      GAS_LIMIT_ETHEREUM_MAINNET: `${7.5e9}`,
-      GAS_LIMIT_ARBITRUM_SEPOLIA: `${25e9}`,
-
-      USE_HW_WALLET_LOCAL: 'true',
-      USE_HW_WALLET_TESTNET: 'false',
-      USE_HW_WALLET_ETHEREUM_MAINNET: 'true',
-
-      DEPLOYER_LOCAL: '0x1111111111111111111111111111111111111111',
-      DEPLOYER_TESTNET: '0x2222222222222222222222222222222222222222',
-      DEPLOYER_ETHEREUM_MAINNET: '0xaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa',
-      DEPLOYER_ARBITRUM_SEPOLIA: '0x0987654321098765432109876543210987654321',
-
-      THRESHOLD_LOCAL: '1',
-      THRESHOLD_TESTNET: '2',
-      THRESHOLD_ETHEREUM_SEPOLIA: '3',
-      THRESHOLD_ETHEREUM_MAINNET: '4',
-      THRESHOLD_ARBITRUM_SEPOLIA: '5',
-    } satisfies NodeJS.ProcessEnv;
-
-    test('should parse deployment environment variables correctly', () => {
-      const parse = (networkName: NetworkName) =>
-        parseDeploymentEnvConfig(
-          deploymentTestSchema,
-          networkName,
-          deploymentTestEnv,
-        );
-
-      expect(parse('ethereum-sepolia')).toEqual({
-        mergedConfig: {
-          NETWORKS: [
-            'local',
-            'ethereum-sepolia',
-            'ethereum-mainnet',
-            'arbitrum-sepolia',
-            'arbitrum-mainnet',
-          ],
-          RETRIES: 3,
-          GAS_LIMIT: 10e9, // fallback to global
-          USE_HW_WALLET: false, // fallback to testnet
-          DEPLOYER: '0x2222222222222222222222222222222222222222', // fallback to testnet
-          THRESHOLD: 3, // network-specific
-        },
-        global: {
-          NETWORKS: [
-            'local',
-            'ethereum-sepolia',
-            'ethereum-mainnet',
-            'arbitrum-sepolia',
-            'arbitrum-mainnet',
-          ],
-          RETRIES: 3,
-          GAS_LIMIT: 10e9,
-        },
-        perNetworkKind: {
-          GAS_LIMIT: null,
-          USE_HW_WALLET: false,
-          DEPLOYER: '0x2222222222222222222222222222222222222222',
-          THRESHOLD: 2,
-        },
-        perNetworkName: {
-          GAS_LIMIT: null,
-          USE_HW_WALLET: null,
-          DEPLOYER: null,
-          THRESHOLD: 3,
-        },
       });
     });
   });
--- libs/ts/base-utils/src/env/functions.ts
+++ libs/ts/base-utils/src/env/functions.ts
@@ -1,7 +1,9 @@
-import { Either, Schema as S } from 'effect';
+import { Either, Pretty, Schema as S } from 'effect';
 
-import { assertNotNull } from '../assert';
-import { kebabToSnakeCase } from '../string';
+import { assert, assertNotNull } from '../assert';
+import { kebabToSnakeCase, camelCaseSnakeCase } from '../string';
+import { UnionToIntersection } from '../type-level';
+import { entriesOf, fromEntries, keysOf } from '../array-iter';
 
 /**
  * Retrieves the value of an environment variable.
@@ -32,6 +34,7 @@ export function getEnvStringNotAssert(varName: string): string {
   }
   return value;
 }
+
 export function parseRequiredEnv<T, S extends string>(
   varName: string,
   schema: VarSchema<T, S>,
@@ -58,7 +61,9 @@ export function parseEnv<T, S extends string>(
 
   if (Either.isLeft(value)) {
     if (env[varName] == null) {
-      if (optional) return null;
+      if (optional) {
+        return null;
+      }
       throw new Error(`Env variable '${varName}' is missing.`);
     }
 
@@ -72,9 +77,9 @@ export function parseEnv<T, S extends string>(
 
 export type VarSchema<T = any, S extends string = string> = S.Schema<T, S>;
 
-export function asEnvSchema<T, S extends string>(
-  schema: VarSchema<T, S>,
-): S.Schema<T, string> {
+export function asVarSchema<T, S extends string>(
+  schema: S.Schema<T, S>,
+): VarSchema<T> {
   return schema as S.Any as VarSchema<T>;
 }
 
@@ -94,11 +99,228 @@ export function parseEnvConfig<Env$ extends EnvSchema>(
   const res = {} as EnvTypeFromSchema<Env$>;
 
   for (const key in config) {
-    const varName = kebabToSnakeCase(
+    const varName = camelCaseSnakeCase(
       (suffix ?? '').length ? `${key}_${suffix}` : key,
     );
     res[key] = parseEnv(varName, config[key], true, env);
   }
 
   return res;
+}
+
+export type LayeredEnvSchema<
+  LayerNames extends readonly [string, ...string[]],
+  Layers extends Record<LayerNames[number], EnvSchema>,
+> = {
+  priority: LayerNames;
+  suffixes: Record<LayerNames[number], string>;
+  layers: Layers;
+};
+
+export type LayeredEnvSchemaToConfig<
+  LayerNames extends readonly [string, ...string[]],
+  Layers extends Record<LayerNames[number], EnvSchema>,
+> = {
+  priority: LayerNames;
+  suffixes: Record<LayerNames[number], string>;
+  layers: {
+    [key in LayerNames[number]]: EnvTypeFromSchema<Layers[key]>;
+  };
+  mergedConfig: UnionToIntersection<
+    {
+      [L in keyof LayerNames]: EnvTypeFromSchema<Layers[LayerNames[L]]>;
+    }[number]
+  >;
+  mergedConfigSchema: UnionToIntersection<
+    {
+      [L in keyof LayerNames]: { layer: string; schema: Layers[LayerNames[L]] };
+    }[number]
+  >;
+};
+
+export function parseLayeredEnvConfig<
+  LayerNames extends readonly [string, ...string[]],
+  Layers extends Record<LayerNames[number], EnvSchema>,
+>(
+  config: LayeredEnvSchema<LayerNames, Layers>,
+  env: NodeJS.ProcessEnv = process.env,
+): LayeredEnvSchemaToConfig<LayerNames, Layers> {
+  const schemaLayers = config.priority.map(
+    (layerName: LayerNames[number]) =>
+      [layerName, config.layers[layerName]] as const,
+  );
+  const mergedConfigSchema = mergeSchemaLayers(schemaLayers);
+
+  const mergeSchemaWithoutLayerAnnotations = fromEntries(
+    entriesOf(mergedConfigSchema).map(
+      ([key, { schema }]) => [key, S.NullOr(schema)] as const,
+    ),
+  );
+
+  const result = {
+    mergedConfigSchema,
+    suffixes: config.suffixes,
+    priority: config.priority,
+    layers: {},
+    mergedConfig: {},
+  } as LayeredEnvSchemaToConfig<LayerNames, Layers>;
+
+  const mergedConfig = result.mergedConfig as Record<string, any>;
+  for (const layerName_ of config.priority) {
+    const layerName = layerName_ as LayerNames[number];
+    const layer = config.layers[layerName];
+    const parsedLayer = parseEnvConfig(layer, config.suffixes[layerName], env);
+    result.layers[layerName] = parsedLayer;
+
+    for (const key of keysOf(layer)) {
+      if (mergedConfig[key] == null) {
+        (result.mergedConfig as any)[key] = parsedLayer[key];
+      }
+    }
+  }
+
+  // Validate the merged configuration against the schema
+  const validationResult = S.validateEither(
+    S.Struct(mergeSchemaWithoutLayerAnnotations),
+  )(result.mergedConfig);
+
+  if (Either.isLeft(validationResult)) {
+    throw new Error(
+      'Merged configuration is invalid:\n' + validationResult.left,
+    );
+  }
+
+  return result;
+}
+
+export type MergedConfigSchema = Record<
+  string,
+  { layer: string; schema: VarSchema }
+>;
+
+function mergeSchemaLayers(
+  layers: Iterable<readonly [string, EnvSchema]>,
+): MergedConfigSchema {
+  const mergedConfigSchema = {} as MergedConfigSchema;
+  for (const [layerName, layer] of layers) {
+    for (const key of keysOf(layer)) {
+      const schema = layer[key];
+      if (!(key in mergedConfigSchema)) {
+        mergedConfigSchema[key] = {
+          layer: layerName,
+          schema: schema as VarSchema,
+        };
+      } else {
+        const schemaToJSON = (schema: S.Schema.All) =>
+          JSON.stringify(schema.ast.toJSON(), null, 2);
+        // TODO: find a more performant way to check for schema equality
+        const s1 = schema;
+        const s2 = mergedConfigSchema[key].schema;
+        const s1JSON = schemaToJSON(s1);
+        const s2JSON = schemaToJSON(s2);
+        assert(
+          s1JSON === s2JSON,
+          `Schema for '${key}' is different at different layers:` +
+            `\n  at '${layerName}': '${s1}'` +
+            `\n  at '${mergedConfigSchema[key].layer}': '${s2}'` +
+            (`${s1}` === `${s2}` ? `\n  ${s1JSON}` + `\n  ${s2JSON}` : ''),
+        );
+      }
+    }
+  }
+
+  return mergedConfigSchema;
+}
+
+export function prettyPrintParsedEnvConfig<
+  LayerNames extends readonly [string, ...string[]],
+  Layers extends Record<LayerNames[number], EnvSchema>,
+>(config: LayeredEnvSchemaToConfig<LayerNames, Layers>, tty = true) {
+  const bold = tty ? '\x1B[1m' : '';
+  const noBold = tty ? '\x1B[22m' : '';
+  const underline = tty ? '\x1B[4m' : '';
+  const noUnderline = tty ? '\x1B[24m' : '';
+  const red = tty ? '\x1B[31m' : '';
+  const green = tty ? '\x1B[32m' : '';
+  const noColor = tty ? '\x1B[39m' : '';
+
+  const getColumnWidth = <S extends string>(cells: Iterable<S>) =>
+    Iterator.from(cells).reduce((max, cell) => Math.max(max, cell.length), 0);
+
+  const mergedConfigSchema = config.mergedConfigSchema as MergedConfigSchema;
+  const mergedConfig = config.mergedConfig as Record<string, unknown>;
+  const keys = keysOf(mergedConfigSchema).toArray();
+  const groupedKeys = entriesOf(
+    Object.groupBy(keys, key => mergedConfigSchema[key].layer),
+  )
+    .toArray()
+    .sort(([layerA], [layerB]) => {
+      const indexA = config.priority.indexOf(layerA as LayerNames[number]);
+      const indexB = config.priority.indexOf(layerB as LayerNames[number]);
+      return indexB - indexA;
+    });
+
+  const getVarInfo = (key: string) => {
+    const { layer: layer_, schema } = mergedConfigSchema[key];
+    assert(
+      S.isSchema(schema),
+      `Expected schema for '${key}' but got '${schema}'`,
+    );
+    const layer = layer_ as LayerNames[number];
+    const suffix = config.suffixes[layer];
+    const envVarName = kebabToSnakeCase(
+      suffix.length ? `${key}_${suffix}` : key,
+    );
+    const value = mergedConfig[key];
+    return { key, value, layer, envVarName, schema };
+  };
+
+  const longestKeyLength = getColumnWidth(keys);
+  const longestLayerLength = getColumnWidth(
+    groupedKeys.map(([layer]) => layer),
+  );
+  const columnWidth = Math.max(longestKeyLength, longestLayerLength);
+  const lineLength = 6 + columnWidth;
+  const title = (txt: string) => `╼ ${bold}${txt}${noBold} ╾`;
+
+  const center = (text: string, width: number, padding = '─') => {
+    // calculate text width, strip ANSI codes
+    const textWidth = text.replace(/\x1B\[[0-9;]*m/g, '').length;
+    const leftPadding = Math.max(0, Math.floor((width - textWidth) / 2));
+    const rightPadding = Math.max(0, width - textWidth - leftPadding);
+    return padding.repeat(leftPadding) + text + padding.repeat(rightPadding);
+  };
+
+  const line = (len: number) => '─'.repeat(len);
+  if (tty) '─'.repeat(lineLength);
+  let text = '';
+  text += `╭─${center(title('Env config'), lineLength)}─╮` + '\n';
+  for (const [layer, layerKeys] of groupedKeys) {
+    text += `├─┬${center(title(layer), lineLength)}╮` + '\n';
+    for (const key of assertNotNull(layerKeys)) {
+      text += '│ │';
+      const { envVarName, value, layer, schema } = getVarInfo(key);
+
+      const keyFormatted = `${key.padStart(columnWidth + 2)}: `;
+      const envVarFormatted = `(${bold}${layer}${noBold} env var ${bold}${envVarName}${noBold})`;
+
+      if (value != null) {
+        const prettyPrint = Pretty.make(schema);
+        text +=
+          keyFormatted +
+          `✅ ${bold}${underline}${prettyPrint(value)}${noUnderline}${noBold} ` +
+          `${green}${envVarFormatted}${noColor}`;
+      } else {
+        text +=
+          keyFormatted +
+          `❌ ${red}${underline}missing${noUnderline} ` +
+          `${envVarFormatted}${noColor}`;
+      }
+      text += '\n';
+    }
+    text += `│ └${line(lineLength)}╯` + '\n';
+  }
+  text += `╰${line(lineLength + 2)}╯`;
+
+  console.log(text);
 }
