70e3c9e27cb4d677ebf0683271c41c76412c7adc
Radoslav Dimchev <radoslav.dimchev@blocksense.network>
2025-05-13 15:15:52 +0300

    refactor(nft): Increase text size of `AlertMessage`

--- apps/nft.blocksense.network/components/AlertMessage.tsx
+++ apps/nft.blocksense.network/components/AlertMessage.tsx
@@ -10,7 +10,7 @@ export const AlertMessage = () => {
   return (
     <p
       role="alert"
-      className="alert-message border border-[var(--gray-dark)] rounded-full text-center px-4 py-2 text-xs mt-2"
+      className="alert-message border border-[var(--gray-dark)] rounded-full text-center px-4 py-2 text-lg mt-2"
     >
       {alertMessage}
     </p>
