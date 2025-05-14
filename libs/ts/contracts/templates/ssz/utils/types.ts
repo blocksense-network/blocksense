050b82cbf8db3ee3e7502060e061ea6984a361aa
Aneta Tsvetkova <anetastsvetkova@gmail.com>
2025-05-14 16:08:24 +0300

    refactor(decoder): Rewrite ssz decoder in a cleaner way

---
+++ libs/ts/contracts/templates/ssz/utils/types.ts
@@ -0,0 +1,42 @@
+export type Schema = {
+  isBasic: boolean;
+  isDynamic: boolean;
+  isNested: boolean;
+  type: string;
+  typeName: string;
+  fixedSize: number;
+  sszFixedSize: number | null;
+  fixedEnd: number;
+  isFirst?: boolean;
+  length?: number;
+  fieldName?: string;
+  fields?: Schema[];
+  isFixedLen?: boolean[];
+  fieldRangesFixedLen?: { start: Offset; end: Offset }[];
+  variableOffsetsPosition?: number[];
+  isLastDynamic?: boolean;
+  prevType?: { type: string; length?: number };
+  types: { type: string; length?: number }[];
+};
+
+export type Offset = number | string;
+
+export type BytesRange = {
+  start: {
+    value: Offset;
+    isGenerated: boolean;
+  };
+  end: { value: Offset; isGenerated: boolean };
+};
+
+export const hasFields = (
+  schema: Schema,
+): schema is Schema & { fields: Schema[] } => {
+  return (schema as Schema & { fields: Schema[] }).fields !== undefined;
+};
+
+export const isVector = (
+  schema: Schema,
+): schema is Schema & { length: number } => {
+  return schema.typeName.startsWith('Vector') && schema.length !== undefined;
+};
