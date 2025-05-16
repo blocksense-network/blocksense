a9a85760d267831659697a9c58812c477556f3d5
Emil Ivanichkov <emil.ivanichkov@gmail.com>
2025-05-17 02:46:15 +0300

    refactor(social-verification): Extract retweet check logic to separate function

--- apps/social-verification/src/utils.ts
+++ apps/social-verification/src/utils.ts
@@ -1,5 +1,6 @@
 import { HttpApiSchema } from '@effect/platform';
 import { Schema as S } from 'effect';
+import { RetweetCheckPayload, TweetsResponseSchema } from './types';
 
 export class TooManyRequests extends HttpApiSchema.EmptyError<TooManyRequests>()(
   {
@@ -34,4 +35,51 @@ export function fetchAndDecodeJSON<A, I>(
       return response.json();
     })
     .then(json => S.decodeUnknownSync(schema)(json));
+}
+
+export async function checkCodeRetweet(
+  payload: RetweetCheckPayload,
+  tweetId: string,
+  socialDataApiKey: string,
+) {
+  const { userId, retweetCode } = payload;
+  const xUserRetweetsResponse = await fetchAndDecodeJSON(
+    TweetsResponseSchema,
+    `https://api.socialdata.tools/twitter/user/${userId}/tweets`,
+    {
+      headers: {
+        Authorization: `Bearer ${socialDataApiKey}`,
+      },
+    },
+  );
+  console.log(`Successfully fetched retweet. userId: ${userId}`);
+
+  const targetRetweets = xUserRetweetsResponse.tweets.filter(
+    tweet => tweet.quoted_status?.id_str === tweetId,
+  );
+  if (!targetRetweets || targetRetweets.length === 0) {
+    console.error(
+      `No quoted retweets found for user ${userId} on tweet ${tweetId}`,
+    );
+    return { isRetweeted: false, isCodeCorrect: false };
+  }
+
+  const retweetContainsCode = targetRetweets.some(tweet =>
+    tweet.full_text.includes(retweetCode),
+  );
+  if (!retweetContainsCode) {
+    console.error(
+      `Retweet does not contain the correct code for user ${userId} on tweet ${tweetId}`,
+    );
+    return { isRetweeted: true, isCodeCorrect: false };
+  }
+
+  console.log(
+    `Successfully verified quote retweet for user ${userId} on tweet ${tweetId} with code ${retweetCode}`,
+  );
+
+  return {
+    isRetweeted: true,
+    isCodeCorrect: true,
+  };
 }
