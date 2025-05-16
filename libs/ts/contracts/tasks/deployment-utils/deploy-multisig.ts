e8659b9b2218f250f5599dda47ea8b6054e3263d
Aneta Tsvetkova <anetastsvetkova@gmail.com>
2025-05-16 15:32:54 +0300

    fix(tasks): Fix deployment issues and make reporter multisig name consistent
    Co-authored-by: Kristin Kirkov <kristin.kirkov@metacraft-labs.com>

--- libs/ts/contracts/tasks/deployment-utils/deploy-multisig.ts
+++ libs/ts/contracts/tasks/deployment-utils/deploy-multisig.ts
@@ -51,33 +51,32 @@ task('deploy-multisig', '[UTILS] Deploy multisig contract').setAction(
 
     if (await checkAddressExists(config, safeAddress)) {
       console.log(`  -> ✅ ${type} already deployed!`);
-      return protocolKit.connect({
-        provider: config.rpc,
-        signer,
-        safeAddress,
-        contractNetworks: {
-          [config.network.chainId.toString()]: config.safeAddresses,
-        },
-      });
     } else {
       console.log(`  -> ⏳ ${type} not found, deploying...`);
+
+      const deploymentTransaction =
+        await protocolKit.createSafeDeploymentTransaction();
+
+      const transactionHash = await config.deployer.sendTransaction({
+        to: deploymentTransaction.to,
+        value: BigInt(deploymentTransaction.value),
+        data: deploymentTransaction.data as `0x${string}`,
+      });
+
+      const transactionReceipt = await config.provider.waitForTransaction(
+        transactionHash.hash,
+      );
+
+      console.log('    ✅ Safe deployment tx hash:', transactionReceipt?.hash);
     }
 
-    const deploymentTransaction =
-      await protocolKit.createSafeDeploymentTransaction();
-
-    const transactionHash = await config.deployer.sendTransaction({
-      to: deploymentTransaction.to,
-      value: BigInt(deploymentTransaction.value),
-      data: deploymentTransaction.data as `0x${string}`,
+    return protocolKit.connect({
+      provider: config.rpc,
+      signer,
+      safeAddress,
+      contractNetworks: {
+        [config.network.chainId.toString()]: config.safeAddresses,
+      },
     });
-
-    const transactionReceipt = await config.provider.waitForTransaction(
-      transactionHash.hash,
-    );
-
-    console.log('    ✅ Safe deployment tx hash:', transactionReceipt?.hash);
-
-    return protocolKit.connect({ safeAddress });
   },
 );
