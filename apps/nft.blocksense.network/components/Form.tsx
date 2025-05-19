ca020679f48995ee46603e4684b4bf63f563de5a
Radoslav Dimchev <radoslav.dimchev@blocksense.network>
2025-05-19 17:53:21 +0300

    refactor(nft): Finish the campaign

--- apps/nft.blocksense.network/components/Form.tsx
+++ apps/nft.blocksense.network/components/Form.tsx
@@ -3,9 +3,9 @@
 import { useState } from 'react';
 import { motion } from 'motion/react';
 
-import { MintForm } from './MintForm';
 import { SuccessForm } from './SuccessForm';
 import { MintFormProvider } from '../app/contexts/MintFormContext';
+import { RestForm } from './RestFrom';
 
 export const Form = () => {
   const [showSuccess, setShowSuccess] = useState(false);
@@ -28,14 +28,14 @@ export const Form = () => {
 
   return (
     <section className="form md:py-20 md:px-16 px-5 py-8" id="mint-form">
-      <article className="form__article max-w-[32.75rem] mx-auto flex flex-col items-center justify-center md:gap-12 gap-8">
+      <article className="form__article max-w-[35rem] mx-auto flex flex-col items-center justify-center md:gap-12 gap-8">
         <motion.h2
           className="form__title md:text-center md:px-[0.015rem]"
           initial={{ opacity: 0 }}
           whileInView={{ opacity: 1 }}
           transition={{ duration: 1, delay: 0.2 }}
         >
-          Join the crew and receive your NFT by completing all the steps:
+          🚨 NFT drop complete! 🚨
         </motion.h2>
         <MintFormProvider>
           {!showSuccess && (
@@ -45,7 +45,7 @@ export const Form = () => {
               whileInView={{ opacity: 1, scale: 1 }}
               transition={{ duration: 1, delay: 0.2 }}
             >
-              <MintForm onSuccessAction={onSuccess} />
+              <RestForm />
             </motion.div>
           )}
           {showSuccess && (
