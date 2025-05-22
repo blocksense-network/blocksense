9fc78a87d3df2f8df10d2d248250ac0aaa53b75b
Kalin Hristov <kalinsky.hristov@gmail.com>
2025-05-22 16:58:10 +0300

    feat(nft): WIP Implement scrolling animation

--- apps/nft.blocksense.network/app/page.tsx
+++ apps/nft.blocksense.network/app/page.tsx
@@ -5,6 +5,7 @@ import { Hero } from 'components/Hero';
 import { Form } from 'components/Form';
 import { CTA } from 'components/CTA';
 import { About } from 'components/About';
+import { ProductFeatures } from 'components/ProductFeatures';
 export const metadata: Metadata = {
   title: nftDropConfig.title,
 };
@@ -16,6 +17,7 @@ const NFTDropPage = () => {
       <Form />
       <About />
       <CTA />
+      <ProductFeatures />
     </>
   );
 };
