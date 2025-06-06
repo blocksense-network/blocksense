acdd7797b74e93d86d65414849b94e2d57fa16fc
Emil Ivanichkov <emil.ivanichkov@gmail.com>
2025-06-06 15:41:18 +0300

    refactor(scripts/devshell): Improve UX while using scripts
    The names of the shells are now bolded in the logs.

---
+++ scripts/utils/ansi.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+# ANSI styles
+export RESET="\e[0m"
+export BOLD="\e[1m"
+export DIM="\e[2m"
+export GREEN="\e[32m"
+export YELLOW="\e[33m"
+export RED="\e[31m"
+export BLUE="\e[34m"
+export UNDERLINE="\e[4m"
