299ae4aa287a85ab4b353e5dc01a52edae0d337c
Mila Genova <mila.genova@blocksense.network>
2025-06-12 15:43:56 +0300

    feat(onchain-interactions/CLAggregatorAdapter): Return typified results

--- libs/ts/onchain-interactions/src/CLAggregatorAdapter/index.ts
+++ libs/ts/onchain-interactions/src/CLAggregatorAdapter/index.ts
@@ -36,50 +36,103 @@ export class CLAggregatorAdapter {
     });
   }
 
-  async decimals() {
+  async decimals(): Promise<number> {
     return await this.contract.read.decimals();
   }
 
-  async dataFeedStore() {
+  async dataFeedStore(): Promise<`0x${string}`> {
     return await this.contract.read.dataFeedStore();
   }
 
-  async description() {
+  async description(): Promise<string> {
     return await this.contract.read.description();
   }
 
-  async id() {
+  async id(): Promise<bigint> {
     return await this.contract.read.id();
   }
 
-  async latestAnswer() {
+  async latestAnswer(): Promise<bigint> {
     return await this.contract.read.latestAnswer();
   }
 
-  async latestRound() {
+  async latestRound(): Promise<bigint> {
     return await this.contract.read.latestRound();
   }
 
-  async latestRoundData() {
-    return await this.contract.read.latestRoundData();
+  async latestRoundData(): Promise<RoundData> {
+    const roundDataResult = await this.contract.read.latestRoundData();
+    return parseRoundData(roundDataResult);
   }
 
-  async getRoundData(roundId: bigint) {
-    return await this.contract.read.getRoundData([roundId]);
+  async getRoundData(roundId: bigint): Promise<RoundData> {
+    const roundDataResult = await this.contract.read.getRoundData([roundId]);
+    return parseRoundData(roundDataResult);
   }
 
-  async getCLAggregatorAdapterData() {
-    return await this.client.multicall({
-      contracts: clAdapterAbi
-        .filter(method => method.type === 'function')
-        .filter(method => method.name !== 'getRoundData')
-        .map(method => ({
-          address: this.contractAddress,
-          abi: clAdapterAbi,
-          functionName: method.name,
-        })),
+  async getCLAggregatorAdapterData(): Promise<CLAggregatorAdapterData> {
+    const methods = clAdapterAbi
+      .filter(method => method.type === 'function')
+      .filter(method => method.name !== 'getRoundData')
+      .map(method => ({
+        address: this.contractAddress,
+        abi: clAdapterAbi,
+        functionName: method.name,
+      }));
+    const results = (
+      await this.client.multicall({
+        contracts: methods,
+      })
+    ).map((result, index) => {
+      if (result.status === 'failure') {
+        throw new Error(
+          `Multicall for CLAggregatorAdapter failed for method: ${methods[index].functionName}. Error: ${result.error}`,
+        );
+      }
+      return result;
     });
+    return {
+      dataFeedStore: results[0].result as `0x${string}`,
+      decimals: results[1].result as number,
+      description: results[2].result as string,
+      id: results[3].result as bigint,
+      latestAnswer: results[4].result as bigint,
+      latestRound: results[5].result as bigint,
+      latestRoundData: parseRoundData(
+        results[6].result as readonly [bigint, bigint, bigint, bigint, bigint],
+      ),
+    };
   }
+}
+
+export type RoundData = {
+  roundId: bigint;
+  answer: bigint;
+  startedAt: bigint;
+  updatedAt: bigint;
+  answeredInRound: bigint;
+};
+
+export type CLAggregatorAdapterData = {
+  id: bigint;
+  decimals: number;
+  description: string;
+  dataFeedStore: `0x${string}`;
+  latestAnswer: bigint;
+  latestRound: bigint;
+  latestRoundData: RoundData;
+};
+
+function parseRoundData(
+  roundData: readonly [bigint, bigint, bigint, bigint, bigint],
+): RoundData {
+  return {
+    roundId: roundData[0],
+    answer: roundData[1],
+    startedAt: roundData[2],
+    updatedAt: roundData[3],
+    answeredInRound: roundData[4],
+  };
 }
 
 function getViemChain(network: NetworkName): Chain | undefined {
