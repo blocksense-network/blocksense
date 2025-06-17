bb2c57be1c341470f6523f0f17e5995881086ae9
Yordan Madzhunkov <ymadzhunkov@gmail.com>
2025-06-17 15:16:49 +0300

    feat(oracles/eth-rpc): Remove logic based on feed_id
    * Add method as configuration - exchangeRate() or convertToAssets - both
    standard ways to estimate price from DEX pool
    * Rework eth-rpc oracle to no longer base behavior on feed_id
    * Update all config files
    
    Co-authored-by: Emil Ivanichkov <emil.ivanichkov@gmail.com>

--- nix/filesets.nix
+++ nix/filesets.nix
@@ -23,7 +23,7 @@ with lib.fileset;
       # JSON files must be listed one by one, otherwise changing an
       # unrelated JSON file will cause all Rust derivations to be rebuilt
       (root + "/apps/sequencer_tests/Safe.json")
-      (root + "/apps/oracles/eth-rpc/src/abi/YnETHx.json")
+      (root + "/apps/oracles/eth-rpc/src/abi/VaultABI.json")
       (root + "/apps/oracles/eth-rpc/src/abi/YieldFiyUSD.json")
       (root + "/apps/sequencer_tests/SafeProxyFactory.json")
       (root + "/libs/gnosis_safe/safe_abi.json")
