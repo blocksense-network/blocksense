5ef49d78c79cacd0c4ad48afcb64581219e1c1fd
Petar Kirov <petar.p.kirov@gmail.com>
2025-06-20 01:39:36 +0300

    refactor(contracts/test): Remove unnecessary `clAggregatorAdapterConsumer` wrapper functions

--- libs/ts/contracts/test/examples/CLAggregatorAdapterConsumer.test.ts
+++ libs/ts/contracts/test/examples/CLAggregatorAdapterConsumer.test.ts
@@ -1,7 +1,7 @@
-import { artifacts, ethers } from 'hardhat';
-
-import * as utils from './utils/clAggregatorAdapterConsumer';
+import { ethers } from 'hardhat';
 import { expect } from 'chai';
+import { HardhatEthersSigner } from '@nomicfoundation/hardhat-ethers/signers';
+
 import { deployContract } from '../experiments/utils/helpers/common';
 import { CLAggregatorAdapterConsumer } from '@blocksense/contracts/typechain';
 import {
@@ -13,11 +13,16 @@ import { encodeDataAndTimestamp } from '../utils/helpers/common';
 describe('Example: CLAggregatorAdapterConsumer', function () {
   let clAggregatorAdapter: CLAdapterWrapper;
   let clAggregatorAdapterConsumer: CLAggregatorAdapterConsumer;
+  let caller: HardhatEthersSigner;
+  const latestAnswer = 1234;
+  const timestamp = Date.now();
+  const latestEncodedData = encodeDataAndTimestamp(latestAnswer, timestamp);
 
   beforeEach(async function () {
     const admin = (await ethers.getSigners())[9];
     const sequencer = (await ethers.getSigners())[10];
     const accessControlAdmin = (await ethers.getSigners())[5];
+    caller = (await ethers.getSigners())[6];
 
     const proxy = new UpgradeableProxyADFSWrapper();
     await proxy.init(admin, accessControlAdmin);
@@ -31,8 +36,7 @@ describe('Example: CLAggregatorAdapterConsumer', function () {
     clAggregatorAdapter = new CLAdapterWrapper();
     await clAggregatorAdapter.init('ETH/USD', 8, 3, proxy);
 
-    const value = encodeDataAndTimestamp(1234, Date.now());
-    await clAggregatorAdapter.setFeed(sequencer, value, 1n);
+    await clAggregatorAdapter.setFeed(sequencer, latestEncodedData, 1n);
 
     clAggregatorAdapterConsumer =
       await deployContract<CLAggregatorAdapterConsumer>(
@@ -41,37 +45,32 @@ describe('Example: CLAggregatorAdapterConsumer', function () {
       );
   });
 
-  [
-    { title: 'get decimals', fnName: 'getDecimals' },
-    { title: 'get description', fnName: 'getDescription' },
-    { title: 'get latest answer', fnName: 'getLatestAnswer' },
-    { title: 'get latest round', fnName: 'getLatestRound' },
-    { title: 'get latest round data', fnName: 'getLatestRoundData' },
-  ].forEach(data => {
-    it('Should ' + data.title, async function () {
-      await getAndCompareData([], data.fnName as keyof typeof utils);
-    });
+  it('Should compare results from adapter with those from adapter consumer', async function () {
+    await clAggregatorAdapter.checkDecimals(
+      Number(await clAggregatorAdapterConsumer.getDecimals()),
+    );
+    await clAggregatorAdapter.checkDescription(
+      await clAggregatorAdapterConsumer.getDescription(),
+    );
+    expect(await clAggregatorAdapterConsumer.getLatestAnswer()).to.deep.equal(
+      latestAnswer,
+    );
+    await clAggregatorAdapter.checkLatestAnswer(caller, latestEncodedData);
+    await clAggregatorAdapter.checkLatestRoundId(
+      caller,
+      await clAggregatorAdapterConsumer.getLatestRound(),
+    );
+    expect(await clAggregatorAdapterConsumer.getRoundData(1)).to.deep.equal([
+      /* roundId_: */ 1n,
+      /* answer_: */ latestAnswer,
+      /* startedAt_: */ BigInt(timestamp) / 1000n,
+      /* updatedAt_: */ BigInt(timestamp) / 1000n,
+      /* answeredInRound_: */ 1n,
+    ]);
+    await clAggregatorAdapter.checkLatestRoundData(
+      caller,
+      latestEncodedData,
+      1n,
+    );
   });
-
-  it('Should get round data', async function () {
-    await getAndCompareData([1], 'getRoundData');
-  });
-
-  const getAndCompareData = async (
-    data: any[],
-    functionName: keyof typeof utils,
-  ) => {
-    const contractData = await clAggregatorAdapterConsumer.getFunction(
-      functionName,
-    )(...data);
-
-    const config = {
-      address: clAggregatorAdapter.contract.target,
-      abiJson: (await artifacts.readArtifact('CLAggregatorAdapter')).abi,
-      provider: clAggregatorAdapter.contract.runner!,
-    };
-    const utilData = await utils[functionName](config, ...data);
-
-    expect(contractData).to.deep.equal(utilData);
-  };
 });
