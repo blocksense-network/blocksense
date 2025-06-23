c2d8af4c8c8dcad1a8ea74bc6cef1973ba1025fd
Petar Kirov <petar.p.kirov@gmail.com>
2025-06-24 01:37:35 +0300

    refactor(contracts/test/examples): Fix TS errors

--- libs/ts/contracts/test/experiments/examples/utils/feedStoreConsumer.ts
+++ libs/ts/contracts/test/experiments/examples/utils/feedStoreConsumer.ts
@@ -1,30 +1,31 @@
-import { Addressable, ContractRunner } from 'ethers';
-import { ethers } from 'hardhat';
+import {
+  Addressable,
+  Contract,
+  ContractRunner,
+  solidityPacked,
+  toBigInt,
+} from 'ethers';
 
-interface DataFeedStoreConfig {
-  address: string | Addressable;
-  abiJson: any;
-  provider: ContractRunner;
-}
+type EthersContractParams = [
+  address: string | Addressable,
+  abiJson: any,
+  provider: ContractRunner,
+];
 
 export const getLatestAnswer = async (
-  config: DataFeedStoreConfig,
+  config: EthersContractParams,
   key: number,
 ) => {
-  const historicalDataFeedStore = new ethers.Contract(
-    config.address,
-    config.abiJson,
-    config.provider,
-  );
+  const historicalDataFeedStore = new Contract(...config);
   const data = '0x' + ((key | 0x80000000) >>> 0).toString(16).padStart(8, '0');
 
-  const res = await config.provider.call!({
+  const res = await config[2].call!({
     to: historicalDataFeedStore.target,
     data,
   });
 
   // value is 24 bytes
-  const value = ethers.toBigInt(res.slice(0, 50));
+  const value = toBigInt(res.slice(0, 50));
   // timestamp is the trailing 8 bytes
   const timestamp = Number('0x' + res.slice(50, 66));
 
@@ -32,25 +33,21 @@ export const getLatestAnswer = async (
 };
 
 export const getRoundData = async (
-  config: DataFeedStoreConfig,
+  config: EthersContractParams,
   key: number,
   roundId: number,
 ) => {
-  const historicalDataFeedStore = new ethers.Contract(
-    config.address,
-    config.abiJson,
-    config.provider,
-  );
+  const historicalDataFeedStore = new Contract(...config);
 
   const data = '0x' + ((key | 0x20000000) >>> 0).toString(16).padStart(8, '0');
 
-  const res = await config.provider.call!({
+  const res = await config[2].call!({
     to: historicalDataFeedStore.target,
-    data: ethers.solidityPacked(['bytes4', 'uint256'], [data, roundId]),
+    data: solidityPacked(['bytes4', 'uint256'], [data, roundId]),
   });
 
   // value is 24 bytes
-  const value = ethers.toBigInt(res.slice(0, 50));
+  const value = toBigInt(res.slice(0, 50));
   // timestamp is the trailing 8 bytes
   const timestamp = Number('0x' + res.slice(50, 66));
 
@@ -58,43 +55,35 @@ export const getRoundData = async (
 };
 
 export const getLatestRound = async (
-  config: DataFeedStoreConfig,
+  config: EthersContractParams,
   key: number,
 ) => {
-  const historicalDataFeedStore = new ethers.Contract(
-    config.address,
-    config.abiJson,
-    config.provider,
-  );
+  const historicalDataFeedStore = new Contract(...config);
   const data = '0x' + ((key | 0x40000000) >>> 0).toString(16).padStart(8, '0');
 
-  const res = await config.provider.call!({
+  const res = await config[2].call!({
     to: historicalDataFeedStore.target,
     data,
   });
 
   const round = '0x' + res.slice(66);
 
-  return ethers.toBigInt(round);
+  return toBigInt(round);
 };
 
 export const getLatestRoundData = async (
-  config: DataFeedStoreConfig,
+  config: EthersContractParams,
   key: number,
 ) => {
-  const historicalDataFeedStore = new ethers.Contract(
-    config.address,
-    config.abiJson,
-    config.provider,
-  );
+  const historicalDataFeedStore = new Contract(...config);
   const data = '0x' + ((key | 0xc0000000) >>> 0).toString(16).padStart(8, '0');
 
-  const res = await config.provider.call!({
+  const res = await config[2].call!({
     to: historicalDataFeedStore.target,
     data,
   });
 
-  const value = ethers.toBigInt(res.slice(0, 50));
+  const value = toBigInt(res.slice(0, 50));
   const timestamp = Number('0x' + res.slice(50, 66));
   const round = Number('0x' + res.slice(66));
 
