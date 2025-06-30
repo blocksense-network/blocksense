625ccd7ca9a68cfad56597a6ed564939c806a40b
Kalin Hristov <kalinsky.hristov@gmail.com>
2025-06-30 18:38:14 +0300

    build(ui/storybook): Init Storybook configuration

---
+++ libs/ts/ui/.storybook/main.ts
@@ -0,0 +1,23 @@
+import type { StorybookConfig } from '@storybook/nextjs';
+import { createRequire } from 'module';
+import { dirname, join } from 'path';
+
+const require = createRequire(import.meta.url);
+
+function getAbsolutePath(value: string): string {
+  return dirname(require.resolve(join(value, 'package.json')));
+}
+
+const config: StorybookConfig = {
+  stories: ['../src/**/*.mdx', '../src/**/*.stories.@(js|jsx|mjs|ts|tsx)'],
+  addons: [
+    getAbsolutePath('@chromatic-com/storybook'),
+    getAbsolutePath('@storybook/addon-docs'),
+  ],
+  framework: {
+    name: getAbsolutePath('@storybook/nextjs'),
+    options: {},
+  },
+  staticDirs: ['../public'],
+};
+export default config;
