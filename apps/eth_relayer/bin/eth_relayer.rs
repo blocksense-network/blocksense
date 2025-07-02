191ba1c9ee223e075a8a4cad6cb3526270b12adb
Hristo Staykov <izo8611@gmail.com>
2025-07-02 17:19:07 +0300

    Added kafka reader

--- apps/eth_relayer/bin/eth_relayer.rs
+++ apps/eth_relayer/bin/eth_relayer.rs
@@ -1,8 +1,9 @@
 use anyhow::Result;
+use eth_relayer::updates_reader::print_hello;
 
 #[tokio::main]
 async fn main() -> Result<()> {
-    println!("Yo");
-
+    println!("Hello");
+    print_hello();
     Ok(())
 }
