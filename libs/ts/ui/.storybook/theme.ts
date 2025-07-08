3f47c8f6c380a26343aad93ba8b621aa3b831444
Radoslav Dimchev <radoslav.dimchev@blocksense.network>
2025-07-08 19:17:25 +0300

    wip

---
+++ libs/ts/ui/.storybook/theme.ts
@@ -0,0 +1,13 @@
+import { create } from 'storybook/theming';
+
+export default create({
+  base: 'dark',
+
+  fontBase: '"Geist", sans-serif',
+  fontCode: 'monospace',
+
+  brandTitle: 'Blocksense Network',
+  brandUrl: 'https://blocksense.network',
+  brandImage: 'src/assets/logos/primary-black.svg',
+  brandTarget: '_target',
+});
