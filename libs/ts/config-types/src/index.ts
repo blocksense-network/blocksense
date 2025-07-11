121b85838c42ab514cafc2ee9038c6302fddb379
Daniel Stoyanov <danielzstoyanov@gmail.com>
2025-07-11 16:21:13 +0300

    feat(config-types): Init node and dfcg-artifacts related configuration utils

--- libs/ts/config-types/src/index.ts
+++ libs/ts/config-types/src/index.ts
@@ -2,3 +2,5 @@ export * from './chainlink-compatibility';
 export * from './data-feeds-config';
 export * from './evm-contracts-deployment';
 export * from './read-write-config';
+export * from './node-config';
+export * from './dfcg/artifacts';
