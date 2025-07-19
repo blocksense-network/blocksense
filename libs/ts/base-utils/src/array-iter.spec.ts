9704c40c1fe114e78f2917692810fcfa4abdad8d
Petar Kirov <petar.p.kirov@gmail.com>
2025-07-19 21:04:48 +0300

    test(base-utils/array-iter): Improve test coverage

--- libs/ts/base-utils/src/array-iter.spec.ts
+++ libs/ts/base-utils/src/array-iter.spec.ts
@@ -1,5 +1,12 @@
 import { describe, expect, test } from 'vitest';
-import { arrayToObject } from './array-iter';
+import {
+  arrayToObject,
+  entriesOf,
+  fromEntries,
+  keysOf,
+  tuple,
+  valuesOf,
+} from './array-iter';
 
 describe('`base-utils/array-iter` tests', () => {
   describe('arrayToObject', () => {
@@ -48,6 +55,63 @@ describe('`base-utils/array-iter` tests', () => {
         x: { data: { foo: 'bar' } },
         y: { data: { foo: 'baz' } },
       });
+    });
+  });
+
+  describe('keysOf', () => {
+    test('should return an array of a given object keys', () => {
+      const obj = { a: 1, b: '2', c: true };
+      expect(keysOf(obj)).toEqual(['a', 'b', 'c']);
+    });
+
+    test('should return an empty array for an empty object', () => {
+      expect(keysOf({})).toEqual([]);
+    });
+  });
+
+  describe('valuesOf', () => {
+    test('should return an array of a given object values', () => {
+      const obj = { a: 1, b: '2', c: true };
+      expect(valuesOf(obj)).toEqual([1, '2', true]);
+    });
+
+    test('should return an empty array for an empty object', () => {
+      expect(valuesOf({})).toEqual([]);
+    });
+  });
+
+  describe('entriesOf', () => {
+    test('should return an array of a given object entries', () => {
+      const obj = { a: 1, b: '2' };
+      expect(entriesOf(obj)).toEqual([
+        ['a', 1],
+        ['b', '2'],
+      ]);
+    });
+
+    test('should return an empty array for an empty object', () => {
+      expect(entriesOf({})).toEqual([]);
+    });
+  });
+
+  describe('fromEntries', () => {
+    test('should transform a list of key-value pairs into an object', () => {
+      const entries: [string, number][] = [
+        ['a', 1],
+        ['b', 2],
+      ];
+      expect(fromEntries(entries)).toEqual({ a: 1, b: 2 });
+    });
+
+    test('should return an empty object for an empty array', () => {
+      expect(fromEntries([])).toEqual({});
+    });
+  });
+
+  describe('tuple', () => {
+    test('should create a tuple from arguments', () => {
+      expect(tuple('a')).toEqual(['a']);
+      expect(tuple('a', 'b', 1)).toEqual(['a', 'b', 1]);
     });
   });
 });
