6c3c9ab452712a009006d2cdd7fe206784891701
Petar Kirov <petar.p.kirov@gmail.com>
2025-07-21 11:14:39 +0300

    fix(avm-relayer): Fix build and skip tests if PXE_URL is not defined

--- apps/avm-relayer/test/relayer.test.ts
+++ apps/avm-relayer/test/relayer.test.ts
@@ -1,18 +1,31 @@
-import { beforeAll, describe, expect, test } from '@effect/vitest';
-import type { AccountWallet, PXE } from '@aztec/aztec.js';
+import { beforeAll, describe, expect, test, assert } from '@effect/vitest';
+import * as S from 'effect/Schema';
+import {
+  createPXEClient,
+  waitForPXE,
+  type AccountWallet,
+  type PXE,
+} from '@aztec/aztec.js';
 import { getInitialTestAccountsWallets } from '@aztec/accounts/testing';
-import { setupSandbox } from '../src/utils';
 
-let pxe: PXE;
-let _wallets: Array<AccountWallet> = [];
+const pxeUrl = S.is(S.URL)(process.env['PXE_URL'])
+  ? process.env['PXE_URL']
+  : null;
 
-beforeAll(async () => {
-  pxe = await setupSandbox();
+describe.skipIf(!pxeUrl)('AVM Relayer', () => {
+  let pxe: PXE;
+  let wallets: Array<AccountWallet> = [];
 
-  _wallets = await getInitialTestAccountsWallets(pxe);
-});
+  beforeAll(async () => {
+    assert(pxeUrl, 'PXE_URL must be set for AVM Relayer tests');
+    pxe = createPXEClient(pxeUrl);
+    await waitForPXE(pxe);
+    wallets = await getInitialTestAccountsWallets(pxe);
+    console.log(
+      `Available wallets: ${wallets.map(w => w.getAddress()).join('\n')}`,
+    );
+  });
 
-describe('AVM Relayer', () => {
   test('should pass', () => {
     expect(true).toBe(true);
   });
