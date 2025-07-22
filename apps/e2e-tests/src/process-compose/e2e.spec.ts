f069f186d66149a5f54f4cd5739de8d5a0aa98d1
Mila Genova <mila.genova@blocksense.network>
2025-07-22 15:59:26 +0300

    feat(e2e-tests): Collect feed ids from feeds config if `allow-feeds` is empty array

--- apps/e2e-tests/src/process-compose/e2e.spec.ts
+++ apps/e2e-tests/src/process-compose/e2e.spec.ts
@@ -7,8 +7,14 @@ import { getProcessComposeLogsFiles } from '@blocksense/base-utils/env';
 import { fetchAndDecodeJSON } from '@blocksense/base-utils/http';
 import { entriesOf, mapValuePromises } from '@blocksense/base-utils';
 import { AggregatedDataFeedStoreConsumer } from '@blocksense/contracts/viem';
-import type { SequencerConfigV2 } from '@blocksense/config-types/node-config';
-import { SequencerConfigV2Schema } from '@blocksense/config-types/node-config';
+import type {
+  SequencerConfigV2,
+  NewFeedsConfig,
+} from '@blocksense/config-types';
+import {
+  SequencerConfigV2Schema,
+  NewFeedsConfigSchema,
+} from '@blocksense/config-types';
 
 import {
   parseProcessesStatus,
@@ -19,11 +25,13 @@ import { expectedPCStatuses03 } from './expected';
 
 describe.sequential('E2E Tests with process-compose', async () => {
   const sequencerConfigUrl = 'http://127.0.0.1:5553/get_sequencer_config';
+  const sequencerFeedsConfigUrl = 'http://127.0.0.1:5553/get_feeds_config';
   const network = 'ink_sepolia';
 
   let sequencerConfig: SequencerConfigV2;
+  let feedsConfig: NewFeedsConfig;
   let ADFSConsumer;
-  let allowFeeds: Array<bigint>;
+  let feedIds: Array<bigint>;
   let initialPrices: Record<string, number>;
 
   beforeAll(async () => {
@@ -58,23 +66,29 @@ describe.sequential('E2E Tests with process-compose', async () => {
       sequencerConfigUrl,
     );
 
+    feedsConfig = await fetchAndDecodeJSON(
+      NewFeedsConfigSchema,
+      sequencerFeedsConfigUrl,
+    );
+
     expect(sequencerConfig).toBeTypeOf('object');
 
     // Collect initial information for the feeds and their prices
     const url = sequencerConfig.providers[network].url;
     const contractAddress = sequencerConfig.providers[network]
       .contract_address as `0x${string}`;
-    allowFeeds =
-      sequencerConfig?.providers?.[network]?.allow_feeds?.map(feedId =>
-        BigInt(feedId),
-      ) ?? [];
+    const allow_feeds = sequencerConfig.providers[network].allow_feeds;
+
+    feedIds = allow_feeds?.length
+      ? (allow_feeds as Array<bigint>)
+      : feedsConfig.feeds.map(feed => feed.id);
 
     ADFSConsumer = AggregatedDataFeedStoreConsumer.createConsumerByRpcUrl(
       contractAddress,
       url,
     );
 
-    initialPrices = allowFeeds.reduce(
+    initialPrices = feedIds.reduce(
       (acc, feedId) => {
         acc[feedId.toString()] = 0;
         return acc;
