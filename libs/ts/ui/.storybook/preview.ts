e9c6aa73dc7df9c24ac5045acaa616f85946837a
Kalin Hristov <kalinsky.hristov@gmail.com>
2025-07-29 14:40:51 +0300

    feat(ui/storybook): Update background colors
    - Update light and dark background colors to match our brand

--- libs/ts/ui/.storybook/preview.ts
+++ libs/ts/ui/.storybook/preview.ts
@@ -9,6 +9,12 @@ const preview: Preview = {
         date: /Date$/i,
       },
     },
+    backgrounds: {
+      options: {
+        light: { name: 'Light', value: '#FFFBFA' },
+        dark: { name: 'Dark', value: '#1F1F1F' },
+      },
+    },
     layout: 'centered',
   },
   tags: ['autodocs'],
