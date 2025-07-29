ee286bb33f517f042aefe1ac73275ed72cc3266e
Kalin Hristov <kalinsky.hristov@gmail.com>
2025-07-29 18:26:56 +0300

    feat(ui/storybook): Update theme
    - Update storybook theme to match our brand

--- libs/ts/ui/.storybook/theme.ts
+++ libs/ts/ui/.storybook/theme.ts
@@ -10,4 +10,29 @@ export default create({
   brandUrl: 'https://blocksense.network',
   brandImage: '/images/primary-black.svg',
   brandTarget: '_target',
+
+  // Colors
+  colorPrimary: '#EEFF00', // Blocksense Yellow
+  colorSecondary: '#1A57FF', // Blocksense Blue
+
+  // UI
+  appBg: '#262626', // Sidebar background (Neutral 1000)
+  appContentBg: '#2C2C2C', // Main content background (Neutral 900)
+  appBorderColor: '#484848', // Border color (Neutral 750)
+  appBorderRadius: 4,
+
+  // Text colors
+  textColor: '#FFFBFA', // Blocksense White
+  textInverseColor: '#1F1F1F', // Blocksense Black
+
+  // Toolbar default and active colors
+  barTextColor: '#D2D2D2', // Neutral 125
+  barSelectedColor: '#EEFF00', // Blocksense Yellow
+  barBg: '#333333', // Neutral 850
+
+  // Form colors
+  inputBg: '#3A3A3A', // Neutral 800
+  inputBorder: '#5D5D5D', // Neutral 650
+  inputTextColor: '#FFFBFA', // Blocksense White
+  inputBorderRadius: 4,
 });
