fc150cb734d388263d8bbf9722250f9baa800df5
HusseinAdeiza <yunusahusseinadeiza@gmail.com>
2025-08-06 15:35:06 +0100

    fix: improve error message and correct Tailwind CSS plugin config

--- apps/nft.blocksense.network/app/error.tsx
+++ apps/nft.blocksense.network/app/error.tsx
@@ -8,7 +8,7 @@ const ErrorPage = () => {
   return (
     <article className="error-page h-[calc(100vh-19rem)] flex flex-col justify-center items-center p-6 gap-4 w-full">
       <h1 className="error-page__title">500 - Internal Server Error</h1>
-      <p className="error-page__message">Something happened, try again later</p>
+      <p className="error-page__message">Something went wrong. Please try again later.</p>
       <Link href="/" className="cta__link">
         <Button variant="primary" className="cta__button">
           Back to Home
