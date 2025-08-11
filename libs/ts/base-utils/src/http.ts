6cd674a48af8054e997f04f939cd4e33c9f70afb
Daniel Stoyanov <danielzstoyanov@gmail.com>
2025-08-11 12:08:58 +0300

    feat(base-utils): Implement `fetchAndDecodeJsonEffect` function

--- libs/ts/base-utils/src/http.ts
+++ libs/ts/base-utils/src/http.ts
@@ -1,4 +1,8 @@
-import { Schema as S } from 'effect';
+import { HttpClientRequest } from '@effect/platform';
+import { HttpClient } from '@effect/platform/HttpClient';
+import { HttpClientError } from '@effect/platform/HttpClientError';
+import { Effect, Schema as S } from 'effect';
+import { ParseError } from 'effect/ParseResult';
 
 export function fetchAndDecodeJSON<A, I>(
   schema: S.Schema<A, I>,
@@ -21,3 +25,23 @@ export function fetchAndDecodeJSON<A, I>(
     })
     .then(json => S.decodeUnknownSync(schema)(json));
 }
+
+export const fetchAndDecodeJSONEffect = <A, I>(
+  schema: S.Schema<A, I>,
+  url: string,
+): Effect.Effect<A, HttpClientError | ParseError, HttpClient> => {
+  return Effect.gen(function* () {
+    const request = HttpClientRequest.get(url).pipe(
+      HttpClientRequest.setHeaders({
+        'Content-Type': 'application/json; charset=UTF-8',
+      }),
+    );
+    const client = yield* HttpClient;
+
+    const json = yield* client
+      .execute(request)
+      .pipe(Effect.flatMap(res => res.json));
+
+    return yield* S.decodeUnknown(schema)(json);
+  });
+};
