89d6d5a761e660a0819b97ee28dcffb3b5a2e5b0
Simeon Armenchev <monyarm@gmail.com>
2025-08-27 13:58:34 +0300

    refactor(nix/machines): Simplify testEnvironmentConfig structure and add system parameter

--- nix/machines/default.nix
+++ nix/machines/default.nix
@@ -5,29 +5,16 @@
 }:
 let
   testEnvironmentConfig =
-    conf:
+    conf: system:
     lib.nixosSystem {
       modules = [
+        self.nixosModules.blocksense-systemd
+        conf
+        self.nixosModules.example-setup-vm
         {
-          imports = [
-            self.nixosModules.blocksense-systemd
-            conf
-            self.nixosModules.example-setup-vm
-            {
-              nixpkgs.hostPlatform = {
-                system = "x86_64-linux";
-              };
-              boot.loader = {
-                systemd-boot.enable = true;
-                efi.canTouchEfiVariables = true;
-                grub.devices = [ "nodev" ];
-              };
-              fileSystems."/" = {
-                device = "/dev/sda1234";
-                fsType = "ext4";
-              };
-            }
-          ];
+          nixpkgs.hostPlatform = {
+            inherit system;
+          };
         }
       ];
     };
@@ -35,6 +22,15 @@ let
 in
 {
   flake.nixosModules.example-setup-vm = {
+    boot.loader = {
+      systemd-boot.enable = true;
+      efi.canTouchEfiVariables = true;
+      grub.devices = [ "nodev" ];
+    };
+    fileSystems."/" = {
+      device = "/dev/sda1234";
+      fsType = "ext4";
+    };
     nix.settings.sandbox = false;
     networking.useNetworkd = true;
     systemd = {
@@ -45,7 +41,7 @@ in
     environment.defaultPackages = [ self.legacyPackages.x86_64-linux.foundry ];
   };
   flake.nixosConfigurations = {
-    example-setup-01 = testEnvironmentConfig ../test-environments/example-setup-01.nix;
-    # example-setup-02 = testEnvironmentConfig ../test-environments/example-setup-02.nix; #services.kafka doesn't exist
+    example-setup-01 = testEnvironmentConfig ../test-environments/example-setup-01.nix "x86_64-linux";
+    # example-setup-02 = testEnvironmentConfig ../test-environments/example-setup-02.nix "x86_64-linux"; #services.kafka doesn't exist
   };
 }
