blocksense-network/blocksense

commit eb06614ba0f8c706d42b26d1891f92e23f3c87ba

feat(nix): Make shells pure again!
- Set `devenv.root` in a pure way (using a local flake input override)
- Rewrite all `root` directory references:
  - runtime (use `devenv.root` or `$GITROOT`, only the latter for now)
  - build-time (relative `../../..`-style, for now)
- `FEEDS_CONFIG_DIR` is now set from outside (in the `Justfile` recipes)
  (not hardcoded in `process-compose`'s config)

nix/test-environments/example-setup-02.nix
@@ -10,7 +10,7 @@
   services.kafka.enable = true;
 
   services.blocksense = {
-    logsDir = lib.mkForce "$GIT_ROOT/logs/process-compose/example-setup-02";
+    logsDir = lib.mkForce "$GIT_ROOT/logs/blocksense/example-setup-02";
     sequencer = {
       kafka-report-endpoint = lib.mkForce "127.0.0.1:9092";
       providers = {

nix/test-environments/example-setup-03.nix
@@ -1,5 +1,4 @@
 {
-  config,
   lib,
   ...
 }:
@@ -17,14 +16,16 @@ let
     in
     asInts;
 
+  root = ../..;
+
   availablePorts =
     let
-      filePath = "${config.devenv.root}/config/generated/process-compose/available-ports";
+      filePath = lib.path.append root "config/generated/process-compose/available-ports";
     in
     if builtins.pathExists filePath then readPortsFromFile filePath else [ 8547 ];
 
-  testKeysDir = config.devenv.root + "/nix/test-environments/test-keys";
-  deploymentV2FilePath = config.devenv.root + "/config/evm_contracts_deployment_v2/ink-sepolia.json";
+  testKeysDir = lib.path.append root "nix/test-environments/test-keys";
+  deploymentV2FilePath = lib.path.append root "config/evm_contracts_deployment_v2/ink-sepolia.json";
 
   upgradeableProxyADFSContractAddressInk =
     (readJson deploymentV2FilePath).contracts.coreContracts.UpgradeableProxyADFS.address;
@@ -39,7 +40,7 @@ in
   services.kafka.enable = lib.mkForce false;
 
   services.blocksense = {
-    logsDir = lib.mkForce "$GIT_ROOT/logs/process-compose/example-setup-03";
+    logsDir = lib.mkForce "$GIT_ROOT/logs/blocksense/example-setup-03";
 
     blama.enable = lib.mkForce false;
 

.envrc
@@ -8,7 +8,11 @@ fi
 
 dotenv_if_exists
 
-use flake ".#${DEV_SHELL:-default}" --impure
+mkdir -p "$PWD/.devenv"
+DEVENV_ROOT_FILE="$PWD/.devenv/root"
+printf %s "$PWD" > "$DEVENV_ROOT_FILE"
+use flake ".#${DEV_SHELL:-default}" --override-input devenv-root "file+file://$DEVENV_ROOT_FILE"
+
 if [[ "$NIX_DIRENV_DID_FALLBACK" = 1 ]]; then
   exit 1
 fi

Justfile
@@ -47,6 +47,7 @@ build-environment environment="all" use-local-cargo-result="0":
   # Collect free ports that process-compose will use
   mkdir -p "$DEST_DIR"
   scripts/utils/collect-available-ports.sh "$DEST_DIR/available-ports"
+  git add --intent-to-add --force "$DEST_DIR/available-ports"
 
   SRC_DIR=$(
     nix build --no-warn-dirty --impure -L --print-out-paths \

flake.nix
@@ -23,6 +23,11 @@
     flake-parts.follows = "mcl-blockchain/flake-parts";
     fenix.follows = "mcl-blockchain/fenix";
     devenv.follows = "mcl-blockchain/devenv";
+    # HACK: <https://github.com/cachix/devenv/issues/1764>
+    devenv-root = {
+      url = "file+file:///dev/null";
+      flake = false;
+    };
     nix2container.follows = "mcl-blockchain/nix2container";
     mk-shell-bin.url = "github:rrbutani/nix-mk-shell-bin";
     blama = {
@@ -33,17 +38,20 @@
 
   outputs =
     inputs@{ flake-parts, ... }:
-    flake-parts.lib.mkFlake { inherit inputs; } {
-      imports = [
-        # Third-party flake-parts modules
-        inputs.devenv.flakeModule
+    flake-parts.lib.mkFlake { inherit inputs; } (
+      { lib, ... }:
+      {
+        imports = [
+          # Third-party flake-parts modules
+          inputs.devenv.flakeModules.default
 
-        # Local flake-parts modules
-        ./nix
-      ];
-      systems = [
-        "x86_64-linux"
-        "aarch64-darwin"
-      ];
-    };
+          # Local flake-parts modules
+          (lib.path.append ./. "nix")
+        ];
+        systems = [
+          "x86_64-linux"
+          "aarch64-darwin"
+        ];
+      }
+    );
 }

nix/filesets.nix
@@ -8,8 +8,8 @@ with lib.fileset;
 
   rustSrc = rec {
     fileset = unions [
-      (root + "/Cargo.toml")
-      (root + "/Cargo.lock")
+      (lib.path.append root "Cargo.toml")
+      (lib.path.append root "Cargo.lock")
 
       (fileFilter (
         file:
@@ -22,11 +22,11 @@ with lib.fileset;
 
       # JSON files must be listed one by one, otherwise changing an
       # unrelated JSON file will cause all Rust derivations to be rebuilt
-      (root + "/apps/sequencer_tests/Safe.json")
-      (root + "/apps/oracles/eth-rpc/src/abi/VaultABI.json")
-      (root + "/apps/oracles/eth-rpc/src/abi/YieldFiyUSD.json")
-      (root + "/apps/sequencer_tests/SafeProxyFactory.json")
-      (root + "/libs/gnosis_safe/safe_abi.json")
+      (lib.path.append root "apps/sequencer_tests/Safe.json")
+      (lib.path.append root "apps/oracles/eth-rpc/src/abi/VaultABI.json")
+      (lib.path.append root "apps/oracles/eth-rpc/src/abi/YieldFiyUSD.json")
+      (lib.path.append root "apps/sequencer_tests/SafeProxyFactory.json")
+      (lib.path.append root "libs/gnosis_safe/safe_abi.json")
     ];
     src = toSource { inherit root fileset; };
   };

nix/modules/blocksense/backends/process-compose.nix
@@ -59,7 +59,7 @@ let
           timeout_seconds = 30;
         };
         log_configuration = logsConfig;
-        log_location = cfg.logsDir + "/anvil-${name}.log";
+        log_location = "${cfg.logsDir}/anvil-${name}.log";
       };
     }
   ) cfg.anvil;
@@ -83,7 +83,7 @@ let
       name = "blocksense-reporter-${name}";
       value.process-compose =
         let
-          working_dir = toString (/. + config.devenv.state + /blocksense/reporter/${name});
+          working_dir = "$GIT_ROOT/.devenv/state/blocksense/reporter/${name}";
         in
         {
           command = ''
@@ -105,7 +105,7 @@ let
             blocksense-sequencer.condition = "process_healthy";
           };
           log_configuration = logsConfig;
-          log_location = cfg.logsDir + "/reporter-${name}.log";
+          log_location = "${cfg.logsDir}/reporter-${name}.log";
           shutdown.signal = 9;
         };
     }
@@ -119,7 +119,6 @@ let
         fi
         ${mkCargoTargetExePath "sequencer"}
       '';
-
       readiness_probe = {
         exec.command = ''
           curl -fsSL http://127.0.0.1:${toString cfg.sequencer.ports.admin}/health \
@@ -144,7 +143,7 @@ let
         };
       }) cfg.sequencer.providers;
       log_configuration = logsConfig;
-      log_location = cfg.logsDir + "/sequencer.log";
+      log_location = "${cfg.logsDir}/sequencer.log";
     };
   };
 
@@ -154,7 +153,7 @@ let
       shutdown.signal = 9;
       depends_on.kafka.condition = "process_started";
       log_configuration = logsConfig;
-      log_location = cfg.logsDir + "/blockchain-reader.log";
+      log_location = "${cfg.logsDir}/blockchain-reader.log";
     };
   };
 
@@ -164,7 +163,7 @@ let
       shutdown.signal = 9;
       depends_on.kafka.condition = "process_started";
       log_configuration = logsConfig;
-      log_location = cfg.logsDir + "/aggregate-consensus-reader.log";
+      log_location = "${cfg.logsDir}/aggregate-consensus-reader.log";
     };
   };
 
@@ -174,7 +173,7 @@ let
       environment = lib.mapAttrsToList (k: v: "${k}=${v}") cfg.blama.environment;
       shutdown.signal = 9;
       log_configuration = logsConfig;
-      log_location = cfg.logsDir + "/blama.log";
+      log_location = "${cfg.logsDir}/blama.log";
       # TODO: Adequate `readiness_probe`
       # readiness_probe = {};
     };

nix/shells/default.nix
@@ -1,7 +1,11 @@
 { ... }:
 {
   perSystem =
-    { inputs', self', ... }:
+    {
+      inputs',
+      self',
+      ...
+    }:
     let
       createShell =
         {

commit 0c3f32e6e364af71e8dc33908b025fd26f0069f4

fix(config): `process-compose` `logsDir` path correction
- Regression introduced by eb06614ba0f8c706d42b26d1891f92e23f3c87ba

Co-authored-by: reo101 <pavel.atanasov2001@gmail.com>

nix/test-environments/example-setup-02.nix
@@ -10,7 +10,7 @@
   services.kafka.enable = true;
 
   services.blocksense = {
-    logsDir = lib.mkForce "$GIT_ROOT/logs/blocksense/example-setup-02";
+    logsDir = lib.mkForce "$GIT_ROOT/logs/process-compose/example-setup-02";
     sequencer = {
       kafka-report-endpoint = lib.mkForce "127.0.0.1:9092";
       providers = {

nix/test-environments/example-setup-03.nix
@@ -40,7 +40,7 @@ in
   services.kafka.enable = lib.mkForce false;
 
   services.blocksense = {
-    logsDir = lib.mkForce "$GIT_ROOT/logs/blocksense/example-setup-03";
+    logsDir = lib.mkForce "$GIT_ROOT/logs/process-compose/example-setup-03";
 
     blama.enable = lib.mkForce false;
 