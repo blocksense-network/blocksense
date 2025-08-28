b11f42024ccdccf9a14e4c1a50b83068b2d5c95f
Petar Kirov <petar.p.kirov@gmail.com>
2025-08-28 23:54:11 +0300

    feat(changelog-generato): Add markdown-list display mode

--- apps/changelog-generator/src/commands/list-feeds.ts
+++ apps/changelog-generator/src/commands/list-feeds.ts
@@ -1,3 +1,4 @@
+import { getAddressExplorerUrl } from '@blocksense/base-utils/evm';
 import type { TableRow } from '@blocksense/base-utils/tty';
 import { renderTui, drawTable } from '@blocksense/base-utils/tty';
 import {
@@ -16,8 +17,12 @@ export const listFeeds = Command.make(
   {
     dir: Options.directory('dir').pipe(Options.withDefault(configDir)),
     network: Options.choice('network', availableNetworks),
+    displayMode: Options.choice('display-mode', [
+      'table',
+      'markdown-list',
+    ]).pipe(Options.withDefault('table')),
   },
-  ({ dir, network }) =>
+  ({ dir, displayMode, network }) =>
     Effect.gen(function* () {
       const feedConfig = yield* Effect.tryPromise(() =>
         readConfig('feeds_config_v2', dir),
@@ -27,12 +32,18 @@ export const listFeeds = Command.make(
         readEvmDeployment(network, true),
       );
 
-      const rows: Array<TableRow> = feedConfig.feeds
-        .filter(
-          feed => `${feed.id}` in deploymentData.contracts.CLAggregatorAdapter,
-        )
-        .map(feed => {
-          const cl = deploymentData.contracts.CLAggregatorAdapter[`${feed.id}`];
+      const feeds: Array<TableRow> = feedConfig.feeds.reduce((res, feed) => {
+        const f = `${feed.id}`;
+        if (f in deploymentData.contracts.CLAggregatorAdapter) {
+          const cl = deploymentData.contracts.CLAggregatorAdapter[f];
+          return [...res, { feed, cl }];
+        } else {
+          return res;
+        }
+      }, []);
+
+      if (displayMode == 'table') {
+        const rows = feeds.map(({ cl, feed }) => {
           return [
             feed.full_name,
             cl?.address ?? '',
@@ -41,17 +52,29 @@ export const listFeeds = Command.make(
             cl?.quote ?? '',
           ];
         });
+        renderTui(
+          drawTable([...rows], {
+            headers: [
+              'Feed Name',
+              'Address',
+              'Decimals',
+              'Base Address',
+              'Quote Address',
+            ],
+          }),
+        );
+      } else if (displayMode == 'markdown-list') {
+        const rows: string = feeds.map(({ cl, feed }) => {
+          const addr = cl?.address ?? '';
+          const addrLink = `[\`${addr}\`](${getAddressExplorerUrl(network, addr)})`;
+          const docsLink = `https://docs.blocksense.network/docs/data-feeds/feed/${feed.id}#${network}`;
+          return (
+            `* **${feed.full_name}** - ${addrLink}` + '\n' + `  * ${docsLink}`
+          );
+        });
 
-      renderTui(
-        drawTable([...rows], {
-          headers: [
-            'Feed Name',
-            'Address',
-            'Decimals',
-            'Base Address',
-            'Quote Address',
-          ],
-        }),
-      );
+        console.log(`Deployed feeds on ${network}`);
+        for (const row of rows) console.log(row);
+      }
     }),
 );
