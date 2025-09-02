326cb9061cdf54dba1e5a511e47a3b90542d2056
Emil Ivanichkov <emil.ivanichkov@gmail.com>
2025-09-03 00:07:49 +0300

    feat(changelog-generator): Add `add-deployment-config` command

--- apps/changelog-generator/src/Cli.ts
+++ apps/changelog-generator/src/Cli.ts
@@ -3,9 +3,10 @@ import { Command } from '@effect/cli';
 import packageJson from '../package.json';
 
 import { listFeeds } from './commands/list-feeds';
+import { addDeploymentConfig } from './commands/add-deployment-config';
 
 const command = Command.make('changelog-generator').pipe(
-  Command.withSubcommands([listFeeds]),
+  Command.withSubcommands([listFeeds, addDeploymentConfig]),
 );
 
 export const run = Command.run(command, {
