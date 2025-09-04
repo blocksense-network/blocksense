228222d7e5a1ea728bf72009e36f71d221f4217c
Radoslav Dimchev <radoslav.dimchev@blocksense.network>
2025-09-04 17:14:10 +0300

    feat(docs): Add scripts from Webflow website

---
+++ apps/docs.blocksense.network/public/website-scripts/lets-talk.js
@@ -0,0 +1,31 @@
+document.addEventListener('DOMContentLoaded', () => {
+  const form = document.querySelector('.lets-talk_form');
+  const nameInput = document.getElementById('Name');
+  const email = document.getElementById('Email');
+
+  form.addEventListener('submit', async e => {
+    e.preventDefault();
+
+    const emailValue = email.value.trim();
+    if (!emailValue) return;
+
+    try {
+      await fetch(
+        'https://social-verification.blocksense.network/verify/letsTalk/sendEmail',
+        {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json',
+            'x-api-key': 'qe3AkeO3I9fmCszlAgQ5DFLACD1fkFoe',
+          },
+          body: JSON.stringify({
+            email: emailValue,
+            name: nameInput.value.trim(),
+          }),
+        },
+      );
+    } catch (err) {
+      console.error(err);
+    }
+  });
+});
