3ef8bbbe3ad949d53354b7a56a7e2fa123e77df1
Mila Genova <mila.genova@blocksense.network>
2025-09-05 16:49:17 +0300

    feat(changelog-generator): Init `read-cl-adapter` cli command

--- apps/dev/src/Cli.ts
+++ apps/dev/src/Cli.ts
@@ -3,8 +3,11 @@ import { Command } from '@effect/cli';
 import packageJson from '../package.json';
 
 import { listFeeds } from './commands/list-feeds';
+import { readClAdapter } from './commands/read-cl-adapter';
 
-const command = Command.make('dev').pipe(Command.withSubcommands([listFeeds]));
+const command = Command.make('dev').pipe(
+  Command.withSubcommands([listFeeds, readClAdapter]),
+);
 
 export const run = Command.run(command, {
   name: packageJson.description,
