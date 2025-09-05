5779165b018640dd27eaf02d937a86dd5c3ba43f
Emil Ivanichkov <emil.ivanichkov@gmail.com>
2025-09-05 16:01:25 +0300

    refactor(oracles/borrow-rates): Move `MyProvider` and rpc urls to `types`

---
+++ apps/oracles/borrow-rates/src/providers/types.rs
@@ -0,0 +1,19 @@
+pub const RPC_URL_HYPERLIQUID_MAINNET: &str = "https://rpc.hyperliquid.xyz/evm";
+pub const RPC_URL_ETHEREUM_MAINNET: &str = "https://eth.llamarpc.com";
+
+pub type MyProvider = alloy::providers::fillers::FillProvider<
+    alloy::providers::fillers::JoinFill<
+        alloy::providers::Identity,
+        alloy::providers::fillers::JoinFill<
+            alloy::providers::fillers::GasFiller,
+            alloy::providers::fillers::JoinFill<
+                alloy::providers::fillers::BlobGasFiller,
+                alloy::providers::fillers::JoinFill<
+                    alloy::providers::fillers::NonceFiller,
+                    alloy::providers::fillers::ChainIdFiller,
+                >,
+            >,
+        >,
+    >,
+    alloy::providers::RootProvider,
+>;
