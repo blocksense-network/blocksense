0eba6e7f7102e7db00f0cbf01030ad481899183d
Mila Genova <mila.genova@blocksense.network>
2025-09-09 12:10:34 +0300

    refactor(changelog-generator): Rename `changelog generator` to `dev`

--- apps/dev/src/Cli.ts
+++ apps/dev/src/Cli.ts
@@ -4,9 +4,7 @@ import packageJson from '../package.json';
 
 import { listFeeds } from './commands/list-feeds';
 
-const command = Command.make('changelog-generator').pipe(
-  Command.withSubcommands([listFeeds]),
-);
+const command = Command.make('dev').pipe(Command.withSubcommands([listFeeds]));
 
 export const run = Command.run(command, {
   name: packageJson.description,
