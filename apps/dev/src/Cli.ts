6c2b44ac1ae53350026534adf8ec07950b0dd12b
Mila Genova <mila.genova@blocksense.network>
2025-09-10 14:19:09 +0300

    feat(dev/read-adfs): Init `read-adfs` cli command

--- apps/dev/src/Cli.ts
+++ apps/dev/src/Cli.ts
@@ -4,9 +4,10 @@ import packageJson from '../package.json';
 
 import { listFeeds } from './commands/list-feeds';
 import { readClAdapter } from './commands/read-cl-adapter';
+import { readAdfs } from './commands/read-adfs';
 
 const command = Command.make('dev').pipe(
-  Command.withSubcommands([listFeeds, readClAdapter]),
+  Command.withSubcommands([listFeeds, readClAdapter, readAdfs]),
 );
 
 export const run = Command.run(command, {
