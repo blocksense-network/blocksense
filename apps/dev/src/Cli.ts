868fe23b6c89995cd674110a4d7dd4c87b79f4f8
Mila Genova <mila.genova@blocksense.network>
2025-09-19 17:26:52 +0300

    feat(dev/adfs): Implement adfs calldata decoder command

--- apps/dev/src/Cli.ts
+++ apps/dev/src/Cli.ts
@@ -3,8 +3,11 @@ import { Command } from '@effect/cli';
 import packageJson from '../package.json';
 
 import { feeds } from './commands/feeds/feeds';
+import { adfs } from './commands/adfs/adfs';
 
-const command = Command.make('dev').pipe(Command.withSubcommands([feeds]));
+const command = Command.make('dev').pipe(
+  Command.withSubcommands([feeds, adfs]),
+);
 
 export const run = Command.run(command, {
   name: packageJson.description,
