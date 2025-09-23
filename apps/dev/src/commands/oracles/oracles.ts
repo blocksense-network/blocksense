90b2ea3fc40a834f58a64addb19a3455ea324e8b
Mila Genova <mila.genova@blocksense.network>
2025-09-23 17:52:40 +0300

    feat(dev/oracles): Implement `oracles add` command

--- apps/dev/src/commands/oracles/oracles.ts
+++ apps/dev/src/commands/oracles/oracles.ts
@@ -1,7 +1,8 @@
 import { Command } from '@effect/cli';
 
 import { list } from './list';
+import { add } from './add';
 
 export const oracles = Command.make('oracles').pipe(
-  Command.withSubcommands([list]),
+  Command.withSubcommands([list, add]),
 );
