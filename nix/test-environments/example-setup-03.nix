6a7981a8605dcdc8c4eadfb3600e658ff30fecce
Daniel Stoyanov <danielzstoyanov@gmail.com>
2025-09-23 15:25:45 +0300

    feat(nix/modules): Create `anvil` config for `offline-start` mode
    In order to run `anvil` in `offline-start` I have introduced the
    following `anvil` options:
    - `--fork-chain-id` - specify chain id to skip fetching it from remote
      endpoint. This enables offline-start mode
    - `--fork-block-number` - fetch state from a specific block number over
      a remote endpoint
    - `--state` - This is an alias for both --load-state and --dump-state. It
      initializes the chain with the state and block environment stored at
      the file, if it exists, and dumps the chain's state on exit
    - `--state-interval` - Interval in seconds at which the state and block
      environment is to be dumped to disk.

--- nix/test-environments/example-setup-03.nix
+++ nix/test-environments/example-setup-03.nix
@@ -50,6 +50,10 @@ in
         port = anvilInkSepoliaPort;
         chain-id = 99999999999;
         fork-url = "$RPC_URL_INK_SEPOLIA";
+        fork-block-number = 29297247;
+        fork-chain-id = 11155111;
+        state = "${config.devenv.root}/config/generated/process-compose/example-setup-03/anvil/state.json";
+        state-interval = 10;
       };
     };
 
