10e55dc88fff3628dec0d0bcf70e36dbeba14d65
Hristo Staykov <izo8611@gmail.com>
2025-10-23 19:40:34 +0300

    feat(nix/process-compose): Add process-compose setup with 2 sequencers

--- nix/modules/blocksense/module-opts/config-files/default.nix
+++ nix/modules/blocksense/module-opts/config-files/default.nix
@@ -5,16 +5,19 @@
 }@args:
 let
   inherit (import ./reporter-config.nix args) mkReporterConfig;
+  mkSequencerConfig = import ./sequencer-config.nix args;
 
   reporter-configs = lib.mapAttrs' (
     name: opts: lib.nameValuePair "reporter_config_${name}" (mkReporterConfig opts)
   ) cfg.reporters;
 
+  sequencer-configs = {
+    sequencer_config = mkSequencerConfig cfg.sequencer;
+  }
+  // lib.mapAttrs' (
+    name: sequencer: lib.nameValuePair "sequencer_config_${name}" (mkSequencerConfig sequencer)
+  ) cfg.extra-sequencers;
+
   mkModuleSettings = builtins.mapAttrs (_: value: { settings = value; });
 in
-mkModuleSettings (
-  reporter-configs
-  // {
-    "sequencer_config" = import ./sequencer-config.nix args;
-  }
-)
+mkModuleSettings (reporter-configs // sequencer-configs)
