79a5682371e59a9867535a30c0ca99eaa2b46e1a
Aneta Tsvetkova <anetastsvetkova@gmail.com>
2025-10-23 16:44:23 +0300

    feat(dev/decoder): Add CLI tool to generate decoder Solidity contracts from a WIT file

--- apps/dev/src/Cli.ts
+++ apps/dev/src/Cli.ts
@@ -3,12 +3,13 @@ import { Command } from '@effect/cli';
 import packageJson from '../package.json';
 
 import { adfs } from './commands/adfs/adfs';
+import { decoder } from './commands/decoder/decoder';
 import { feeds } from './commands/feeds/feeds';
 import { networks } from './commands/networks/networks';
 import { oracles } from './commands/oracles/oracles';
 
 const command = Command.make('dev').pipe(
-  Command.withSubcommands([feeds, oracles, adfs, networks]),
+  Command.withSubcommands([feeds, oracles, adfs, networks, decoder]),
 );
 
 export const run = Command.run(command, {
