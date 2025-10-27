34e3162a482df4b7c51cfcb7f07e68c18bd37354
Kristin Kirkov <kristin.kirkov@metacraft-labs.com>
2025-10-27 15:47:24 +0200

    feat(chain-interactions): Add balance command

--- apps/chain-interactions/src/Cli.ts
+++ apps/chain-interactions/src/Cli.ts
@@ -2,7 +2,11 @@ import { Command } from '@effect/cli';
 
 import packageJson from '../package.json';
 
-const command = Command.make('<command_name>');
+import { balance } from './commands/balance';
+
+const command = Command.make('chain-interactions').pipe(
+  Command.withSubcommands([balance]),
+);
 
 export const run = Command.run(command, {
   name: packageJson.description,
