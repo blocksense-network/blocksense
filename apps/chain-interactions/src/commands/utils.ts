e56e29410f107cfff98df2a2b7e9407df3b0bcae
Kristin Kirkov <kristin.kirkov@metacraft-labs.com>
2025-10-30 15:03:46 +0200

    feat(chain-interactions): Move startPrometheusServer to utils

---
+++ apps/chain-interactions/src/commands/utils.ts
@@ -0,0 +1,17 @@
+import express from 'express';
+import client from 'prom-client';
+
+import { color as c } from '@blocksense/base-utils/tty';
+
+export const startPrometheusServer = (host: string, port: number): void => {
+  const app = express();
+  app.get('/metrics', async (_req, res) => {
+    res.set('Content-Type', client.register.contentType);
+    res.end(await client.register.metrics());
+  });
+  app.listen(port, host, () => {
+    console.log(
+      c`{blue Prometheus metrics exposed at http://${host}:${port}/metrics}`,
+    );
+  });
+};
