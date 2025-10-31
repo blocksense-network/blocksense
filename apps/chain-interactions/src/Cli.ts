40a800c92ceeada311b21e7fbd7c01e485986361
Kristin Kirkov <kristin.kirkov@metacraft-labs.com>
2025-10-31 15:27:53 +0200

    feat(chain-interactions): Add cost command

--- apps/chain-interactions/src/Cli.ts
+++ apps/chain-interactions/src/Cli.ts
@@ -3,9 +3,10 @@ import { Command } from '@effect/cli';
 import packageJson from '../package.json';
 
 import { balance } from './commands/balance';
+import { cost } from './commands/cost';
 
 const command = Command.make('chain-interactions').pipe(
-  Command.withSubcommands([balance]),
+  Command.withSubcommands([balance, cost]),
 );
 
 export const run = Command.run(command, {
