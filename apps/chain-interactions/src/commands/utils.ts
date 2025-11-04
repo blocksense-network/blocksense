fe4fb2d1e834589475a06d6991167362e0ce7caa
Kristin Kirkov <kristin.kirkov@metacraft-labs.com>
2025-11-04 14:04:33 +0200

    feat(chain-interactions): Add getDefaultSequencerAddress function to utils

--- apps/chain-interactions/src/commands/utils.ts
+++ apps/chain-interactions/src/commands/utils.ts
@@ -1,6 +1,9 @@
 import express from 'express';
 import client from 'prom-client';
 
+import { getOptionalEnvString } from '@blocksense/base-utils/env';
+import type { EthereumAddress } from '@blocksense/base-utils/evm';
+import { parseEthereumAddress } from '@blocksense/base-utils/evm';
 import { color as c } from '@blocksense/base-utils/tty';
 
 export const startPrometheusServer = (host: string, port: number): void => {
@@ -18,4 +21,24 @@ export const startPrometheusServer = (host: string, port: number): void => {
 
 export function filterSmallBalance(balance: string, threshold = 1e-6): number {
   return Number(balance) < threshold ? 0 : Number(balance);
+}
+
+export function getDefaultSequencerAddress(
+  shouldUseMainnetSequencer: boolean,
+): EthereumAddress {
+  if (shouldUseMainnetSequencer) {
+    return parseEthereumAddress(
+      getOptionalEnvString(
+        'SEQUENCER_ADDRESS_MAINNET',
+        '0x1F412F1dBab58E41d37ba31115c811B0fBD10904',
+      ),
+    );
+  } else {
+    return parseEthereumAddress(
+      getOptionalEnvString(
+        'SEQUENCER_ADDRESS_TESTNET',
+        '0xd756119012CcabBC59910dE0ecEbE406B5b952bE',
+      ),
+    );
+  }
 }
