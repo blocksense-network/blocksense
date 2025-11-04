d4ab7711ec6c6f4d09da0516a452315160ddf220
Kristin Kirkov <kristin.kirkov@metacraft-labs.com>
2025-11-04 11:53:07 +0200

    feat(chain-interactions): Add unstuck-transaction command

--- apps/chain-interactions/src/Cli.ts
+++ apps/chain-interactions/src/Cli.ts
@@ -5,9 +5,10 @@ import packageJson from '../package.json';
 import { balance } from './commands/balance';
 import { checkPending } from './commands/check-pending';
 import { cost } from './commands/cost';
+import { unstuckTransaction } from './commands/unstuck-transaction';
 
 const command = Command.make('chain-interactions').pipe(
-  Command.withSubcommands([balance, checkPending, cost]),
+  Command.withSubcommands([balance, checkPending, cost, unstuckTransaction]),
 );
 
 export const run = Command.run(command, {
