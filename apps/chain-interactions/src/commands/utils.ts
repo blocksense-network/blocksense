243f53db15c87cc3e001ddbb84c3a4db8e71b872
Kristin Kirkov <kristin.kirkov@metacraft-labs.com>
2025-11-05 14:41:05 +0200

    feat(chain-interactions): Add getNetworks function to utils

--- apps/chain-interactions/src/commands/utils.ts
+++ apps/chain-interactions/src/commands/utils.ts
@@ -1,10 +1,23 @@
+import { Option } from 'effect';
 import express from 'express';
 import client from 'prom-client';
+import Web3 from 'web3';
 
 import { getOptionalEnvString } from '@blocksense/base-utils/env';
-import type { EthereumAddress } from '@blocksense/base-utils/evm';
-import { parseEthereumAddress } from '@blocksense/base-utils/evm';
+import type {
+  ChainId,
+  EthereumAddress,
+  NetworkName,
+} from '@blocksense/base-utils/evm';
+import {
+  getNetworkNameByChainId,
+  isChainId,
+  parseEthereumAddress,
+  parseNetworkName,
+} from '@blocksense/base-utils/evm';
 import { color as c } from '@blocksense/base-utils/tty';
+
+import { deployedMainnets, deployedTestnets } from './types';
 
 export const startPrometheusServer = (host: string, port: number): void => {
   const app = express();
@@ -41,4 +54,65 @@ export function getDefaultSequencerAddress(
       ),
     );
   }
+}
+
+export function getNetworks(
+  network: Option.Option<string>,
+  rpcUrlInput: Option.Option<URL>,
+  mainnet: boolean,
+): Promise<Array<'unknown' | NetworkName>>;
+export function getNetworks(
+  network: Option.Option<string>,
+  mainnet: boolean,
+): NetworkName[];
+export function getNetworks(
+  network: Option.Option<string>,
+  rpcUrlOrMainnet: Option.Option<URL> | boolean,
+  maybeMainnet?: boolean,
+) {
+  const hasRpcUrlArg = typeof rpcUrlOrMainnet !== 'boolean';
+  const rpcUrlInput = hasRpcUrlArg
+    ? (rpcUrlOrMainnet as Option.Option<URL>)
+    : Option.none<URL>();
+  const mainnet = hasRpcUrlArg
+    ? (maybeMainnet ?? false)
+    : (rpcUrlOrMainnet as boolean);
+
+  let networks: Array<'unknown' | NetworkName> | undefined = mainnet
+    ? deployedMainnets
+    : deployedTestnets;
+  if (Option.isSome(network)) {
+    networks = [parseNetworkName(network.value)];
+  }
+
+  if (!hasRpcUrlArg) {
+    return networks;
+  }
+
+  if (Option.isNone(rpcUrlInput)) {
+    return Promise.resolve(networks);
+  }
+
+  return (async () => {
+    let chainId: number | bigint | undefined;
+    try {
+      const web3 = new Web3(String(rpcUrlInput.value));
+      chainId = await web3.eth.net.getId();
+    } catch (e) {
+      console.error(
+        c`{red Failed to fetch chain ID from (RPC: ${rpcUrlInput.value})}`,
+        (e as Error).message,
+      );
+    }
+    if (chainId !== undefined && isChainId(Number(chainId))) {
+      const chainIdNum = Number(chainId) as ChainId;
+      const networkName = getNetworkNameByChainId(chainIdNum);
+      return [networkName];
+    }
+
+    console.log(
+      c`{red Could not determine network name from chain ID ${String(chainId)}.}`,
+    );
+    return ['unknown'];
+  })();
 }
