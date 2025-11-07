a942cab1c3b276cd115cf5e9b605f55e16a93c39
reo101 <pavel.atanasov2001@gmail.com>
2025-11-07 16:06:10 +0200

    fix(test-environments): Stop using `encoded-feed-id`
    - Option is already removed prior to this

--- nix/test-environments/example-setup-03.nix
+++ nix/test-environments/example-setup-03.nix
@@ -67,12 +67,12 @@ in
           impersonated-anvil-account = impersonationAddress;
           publishing-criteria = [
             {
-              encoded-feed-id = 50000; # USDT / USD Pegged
+              feed-id = 50000; # USDT / USD Pegged
               peg-to-value = 1.00;
               peg-tolerance-percentage = 10.0; # 10% tolerance assures that the price will be pegged
             }
             {
-              encoded-feed-id = 50001; # USDC / USD Pegged
+              feed-id = 50001; # USDC / USD Pegged
               peg-to-value = 1.00;
               peg-tolerance-percentage = 0.000001; # 0.000001% tolerance assures that the price will not be pegged
             }
